88b15d127ca145db6c61b5757eee36fa042168a3
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-19 20:37:24 +0100

    removed test files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import Data from './components/Data';
-import { MyApp } from './components/Form';
-
 
 function App() {
   return (
     <div className="App">
-         <MyApp/>
+          
     </div>
   );
 }
