2dcd4891b506cd80c0715bfaaeecdecdf4fdf926
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-19 20:36:07 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Data from './components/Data';
+import { MyApp } from './components/Form';
+
+
+function App() {
+  return (
+    <div className="App">
+         <MyApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  Form,
+  Field,
+  FieldProps,
+} from 'formik';
+
+interface MyFormValues {
+  firstName: string;
+}
+
+export const MyApp: React.FC<{}> = () => {
+  const initialValues: MyFormValues = { firstName: '' };
+  return (
+    <div>
+      <h1>My Example</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          console.log({ values, actions });
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="First Name" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
