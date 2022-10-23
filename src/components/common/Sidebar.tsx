76b5b012fd68328d6480d2f5c7bf400de20fcb98
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-23 14:44:22 +0100

    working on sidebar

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,25 +1,48 @@
-import { FC  } from "react";
+import { FC } from "react";
+import { AiOutlineHome } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { FaCcDiscover } from "react-icons/fa";
+import { MdOutlineExplore } from "react-icons/md";
+import { BiSearch } from "react-icons/bi";
 
+interface SidebarProps {}
 
+const Sidebar: FC<SidebarProps> = () => {
+  return (
+    <div>
+      <Link to="/">
+        <h1 className="text-xl text-red-600  font-semibold  uppercase">
+          <span>Anime</span>
+          <span className="text-black">Stream</span>
+        </h1>
+      </Link>
 
+      <div className="text-black text-lg font-medium mt-10">MENU</div>
+      <div>
+        <Link to="/">
+          <AiOutlineHome size={30} />
+          <p>Home</p>
+        </Link>
 
-interface SidebarProps  {}
- 
-const Sidebar : FC<SidebarProps> = ( ) => {
-    
-    return (
-          <div>
-           <Link to="/">
-            <h1>
-               <span>Anime</span>
-               <span>Stream</span>
-               </h1>
-           </Link>
-           
-          </div>
-    )
-} 
+        <Link to="/discover">
+          <MdOutlineExplore size={30} />
+          <p>Discover</p>
+        </Link>
 
+        <Link to="/search">
+          <BiSearch size={30} />
+          <p>Search</p>
+        </Link>
+      </div>
 
-export default Sidebar
+      <div className="text-black text-lg font-medium mt-10">LIBRARY</div>
+      <div>
+      <button>
+         
+      </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
