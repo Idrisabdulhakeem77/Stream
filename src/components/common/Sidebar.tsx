c16d3f89bb937a97ffc31f8f71c1b7c3562a7739
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-26 21:05:18 +0100

    adding changes to sidebar

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineHistory, AiFillStar } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { FaCcDiscover } from "react-icons/fa";
 import { MdOutlineExplore } from "react-icons/md";
@@ -7,28 +7,34 @@ import { BiSearch, BiUserCircle } from "react-icons/bi";
 import { BsBookmarkHeart } from "react-icons/bs";
 import { HiOutlineLogin } from "react-icons/hi";
 import { useCurrentViewPort } from "../hooks/useCurrentViewPort";
+import { BsFillEyeFill } from "react-icons/bs";
+import { RiSlideshow3Fill } from "react-icons/ri";
 
 interface SidebarProps {
-  isSidebarOpen : boolean,
-  setIsSidebarOpen : any 
+  isSidebarOpen: boolean;
+  setIsSidebarOpen: any;
 }
 
-const Sidebar: FC<SidebarProps> = ( { isSidebarOpen , setIsSidebarOpen } ) => {
+const Sidebar: FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const { isMobile } = useCurrentViewPort();
   return (
     <>
       <div className="h-screen w-[70vw] md:max-w-[250px]" id="main-div">
         {!isMobile && (
-          <Link to="/">
-            <h1 className="text-xl  font-semibold  uppercase text-black">
+          <Link
+            to="/"
+            className="flex  items-center justify-center mt-4 mb-3 border-black font-semibold"
+          >
+            <BsFillEyeFill size={20} className="mr-1 " />
+            <h1 className="text-xl    uppercase text-black">
               <span>Anime</span>
               <span>Stream</span>
             </h1>
           </Link>
         )}
 
-        {/* <div className="text-black text-lg font- mt-10">MENU</div> */}
-        <div className="flex flex-col gap-4 mt-4 ml-4"> 
+        <div className="text-black text-lg text-center">MENU</div>
+        <div className="flex flex-col gap-4 mt-4 ml-4 px-4">
           <Link
             to="/"
             className="flex gap-1 items-center hover:text-gray-700 transition duration-300"
@@ -54,15 +60,40 @@ const Sidebar: FC<SidebarProps> = ( { isSidebarOpen , setIsSidebarOpen } ) => {
           </Link>
         </div>
 
-        {/* <div className="text-black text-lg font-medium mt-6">LIBRARY</div> */}
-        <div className="flex flex-col gap-4 mt-4 ml-4">
+        <div className="text-black text-lg text-center mt-2">LIBRARY</div>
+        <div className="flex flex-col gap-4 mt-4 ml-4 px-4">
           <button className="flex gap-1 items-center hover:text-gray-700 transition duration-300">
             <BsBookmarkHeart size={25} />
             <p>Bookmarked</p>
           </button>
+
+          <button className="flex gap-1 items-center hover:text-gray-700 transition duration-300">
+            <AiOutlineHistory size={25} />
+            <p>Recent</p>
+          </button>
+
+          <button className="flex gap-1 items-center hover:text-gray-700 transition duration-300">
+            <AiFillStar size={25} />
+            <p>Top Rated</p>
+          </button>
         </div>
 
-        {/* <div className="text-black text-lg font-medium mt-6">GENERAL</div> */}
+        <div className="text-black text-lg font-medium mt-4 text-center">
+          Categories
+        </div>
+        <div className="flex flex-col gap-4 mt-4 ml-4 px-4">
+          {/* <button>
+             <RiSlideshow3Fill size={25}/>
+             <p>Tv Shows</p>
+           </button> */}
+
+          <button className="flex gap-1 items-center hover:text-gray-700 transition duration-300">
+            <AiFillStar size={25} />
+            <p>Top Rated</p>
+          </button>
+        </div>
+
+        {/* <div className="text-black ttext-lg font-medium mt-6">GENERAL</div> */}
         <div className="flex flex-col gap-4 mt-4 ml-4">
           <button className="flex gap-1 items-center ">
             <BiUserCircle size={25} />
