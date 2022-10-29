e3252cd2a20f01e98f17ccb3c7fae8f2c150d6c4
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-29 07:13:35 +0100

    added home service

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -6,7 +6,7 @@ import { API_URL } from "./constants"
 const instance = axios.create({
      baseURL : API_URL ,
      params : {
-         api_key : ""
+         api_key : "166ef97c1dbaca45757f8a7d461d59e0"
      }
 })
 
