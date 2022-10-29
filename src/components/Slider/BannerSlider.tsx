50a146728e5f4ada9868bdc0fc678e900b0bb648
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-29 07:37:17 +0100

    working on bannerslide

--- src/components/Slider/BannerSlider.tsx
+++ src/components/Slider/BannerSlider.tsx
@@ -1,7 +1,24 @@
+import { FC } from "react";
+import { Items } from "../../shared/types";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+interface BannerSliderProps {}
 
-const BannerSlider = () => {
-     
-}
+const BannerSlider: FC<BannerSliderProps> = () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+  );
+};
 
-export default BannerSlider
+export default BannerSlider;
