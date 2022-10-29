c1ba9ca02c34508156ee60138b48d98a42e33fb5
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-29 17:14:43 +0100

    succesffully fetched home data with react-query

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,15 +7,23 @@ import SearchBox from "../components/Common/SearchBox";
 import User from "../components/Common/User";
 import BannerSlider from "../components/Slider/BannerSlider";
 import MainHomeFilms from "../components/Home/MainHomeFilm";
-
-
- 
-
-
+import { useQuery } from "@tanstack/react-query";
+import { HomeFilms } from "../shared/types";
+import { getHomeMovies } from "../services/home";
 
 const Home: FC = () => {
+  const {
+    data: dataMovie,
+    isLoading: isLoadingMovie,
+    isError: isErrorMovie,
+    error: errorMovie,
+  } = useQuery<HomeFilms, Error>(["home movies"], getHomeMovies);
+
+ 
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [currentTab, setCurrentTab] = useState(localStorage.getItem("currentTab"));
+  const [currentTab, setCurrentTab] = useState(
+    localStorage.getItem("currentTab")
+  );
   return (
     <>
       <Title value="Anime Stream" />
@@ -40,61 +48,58 @@ const Home: FC = () => {
 
         <div className="flex-grow  pt-0 pb-7 border-x md:px-[2vw] px-[4vw] md:border-dark min-h-screen">
           <div className="flex justify-between items-center md:items-start">
-           <div className="inline-flex  gap-[40px]  relative pb-3  mt-2 md:mt-4 px-4 text-gray-600 ">
-            <button
-              onClick={() => {
-                setCurrentTab("movie");
-                localStorage.setItem("currentTab", "movie");
-              }}
-              className={`${
-                currentTab === "movie" &&
-                "font-medium  transition duration-200 hover:text-black   after:absolute after:h-[3px] after:w-5 after:bottom-0 after:left-[10%] after:bg-black"
-              }`}
-            >
-              Movie
-            </button>
-            <button
-              onClick={() => {
-                setCurrentTab("tv");
-                localStorage.setItem("currentTab", "tv");
-              }}
-              className={`${
-                currentTab === "tv" &&
-                "font-medium  transition duration-200 hover:text-black   after:absolute after:h-[3px] after:w-5 after:bottom-0 after:left-[45%] after:bg-black"
-              }`}
-            >
-              Tv Shows
-            </button>
-            <button
-              onClick={() => {
-                setCurrentTab("anime");
-                localStorage.setItem("currentTab", "anime");
-              }}
-              className={`${
-                currentTab === "anime" &&
-                "font-medium  transition duration-200 hover:text-black   after:absolute after:h-[3px] after:w-5 after:bottom-0 after:left-[80%] after:bg-black"
-              }`}
-            >
-              Anime
-            </button>
-          </div>
-           <div className="flex items-center md:hidden gap-4" > 
-               <p> Unknown</p>
-               {/* <img src="" alt="user" className="w-7 h-7 rounded-full object-cover" /> */}
+            <div className="inline-flex  gap-[40px]  relative pb-3  mt-2 md:mt-4 px-4 text-gray-600 ">
+              <button
+                onClick={() => {
+                  setCurrentTab("movie");
+                  localStorage.setItem("currentTab", "movie");
+                }}
+                className={`${
+                  currentTab === "movie" &&
+                  "font-medium  transition duration-200 hover:text-black   after:absolute after:h-[3px] after:w-5 after:bottom-0 after:left-[10%] after:bg-black"
+                }`}
+              >
+                Movie
+              </button>
+              <button
+                onClick={() => {
+                  setCurrentTab("tv");
+                  localStorage.setItem("currentTab", "tv");
+                }}
+                className={`${
+                  currentTab === "tv" &&
+                  "font-medium  transition duration-200 hover:text-black   after:absolute after:h-[3px] after:w-5 after:bottom-0 after:left-[45%] after:bg-black"
+                }`}
+              >
+                Tv Shows
+              </button>
+              <button
+                onClick={() => {
+                  setCurrentTab("anime");
+                  localStorage.setItem("currentTab", "anime");
+                }}
+                className={`${
+                  currentTab === "anime" &&
+                  "font-medium  transition duration-200 hover:text-black   after:absolute after:h-[3px] after:w-5 after:bottom-0 after:left-[80%] after:bg-black"
+                }`}
+              >
+                Anime
+              </button>
+            </div>
+            <div className="flex items-center md:hidden gap-4">
+              <p> Unknown</p>
+              {/* <img src="" alt="user" className="w-7 h-7 rounded-full object-cover" /> */}
             </div>
 
             <MainHomeFilms />
-              
           </div>
         </div>
 
-
         <div className="shrink-0 max-w-[300px] w-full hidden lg:block px-6 top-0 sticky ">
-            <User/>
-             <SearchBox />
-         </div>
+          <User />
+          <SearchBox />
+        </div>
       </div>
-       
     </>
   );
 };
