5192c3529d08ade623d34761d593099ec9463837
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-29 18:05:11 +0100

    dealing with bugs in slider component

--- src/components/Slider/BannerSlider.tsx
+++ src/components/Slider/BannerSlider.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Items } from "../../shared/types";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper";
 
 interface BannerSliderProps {
   films: Items[] | undefined;
@@ -15,16 +16,29 @@ const BannerSlider: FC<BannerSliderProps> = ({
 }) => {
   return (
     <>
-    
       {isBannerLoading ? (
         <h2> Loading ohhh</h2>
       ) : (
-        <Swiper autoplay={{ delay: 5000, disableOnInteraction: false }}>
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          navigation
+          
+          slidesPerView={1}
+         
+        >
           {(films as Items[]).map((film, index) => (
-            <SwiperSlide key={index}>
-               
+            <SwiperSlide key={film.id}>
+                 <div className="border-black">
+                    <h2> { film.name }</h2>
+                 </div>
             </SwiperSlide>
           ))}
+
+           {/* <SwiperSlide>Slide 1</SwiperSlide>
+           <SwiperSlide>Slide 2</SwiperSlide>
+           <SwiperSlide>Slide 3</SwiperSlide>
+           <SwiperSlide>Slide 4</SwiperSlide>
+           <SwiperSlide>Slide 5</SwiperSlide> */}
         </Swiper>
       )}
     </>
