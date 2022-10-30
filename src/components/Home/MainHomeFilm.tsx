3405e31a501b1934b63d74472488e92fc8adee7e
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-30 20:11:59 +0100

    added the skeleton component

--- src/components/Home/MainHomeFilm.tsx
+++ src/components/Home/MainHomeFilm.tsx
@@ -22,39 +22,9 @@ const MainHomeFilms: FC<MainHomeFilmProps> = ({
         films={data?.Trending}
         isBannerLoading={isBannerLoading}
         dataDetails={dataDetails}
-      /> */}
-        
-     {/* { isSectionLoading ? "loading" : (
-         Object.entries(data as HomeFilms).filter(section => section[0] !== "Trending").map((section ,index) => {
-            <li key={index}>
-                <h2> {section[0]} </h2>
-                <SectionSlider films={section[1]}/>
-            </li>
-         })
-     )}           */}
+  /> */}
 
-         {/* <SectionSlider films={undefined}/> */}
-             { console.log(isSectionLoading)}
-              
-              { isSectionLoading ? (
-                  <>
-                     { new Array(2).fill("").map((_ , index) => (
-                        <li key={index}>
-                            <SectionSlider films={undefined}/>
-                        </li>
-                     )
-                     ) }
-                    </>
-              ) : (
-                  Object.entries(data as HomeFilms).filter((section) => section[0] !== "Trending").map((section , index) =>  console.log(section) )
-              )}
-
-           {  }
-
-
-          { console.log(Object.entries(data as HomeFilms))}
-
-        
+         
    
     </>
   );
