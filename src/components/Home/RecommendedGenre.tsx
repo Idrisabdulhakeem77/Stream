6dc7ac337477387e8b8a146948aafc03b860e50d
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-01 07:56:13 +0100

    successfully got the genres

--- src/components/Home/RecommendedGenre.tsx
+++ src/components/Home/RecommendedGenre.tsx
@@ -1,11 +1,16 @@
+import { useQuery } from '@tanstack/react-query'
 import {FC} from 'react'
-
+import {getRecommendedGenre} from '../../services/search'
+import {  getRecommendedGenreType } from "../../shared/types"
 
 interface RecommendedGenresProps {
      currentTab : string
 }
 
 const RecommendedGenres: FC<RecommendedGenresProps>  = ( { currentTab  }) => {
+     const  { data , isError , isLoading , error }  = useQuery<getRecommendedGenreType | Error> (["genres"] , getRecommendedGenre) 
+
+     console.log(data)
       return (
          <div>
              Recommended Genre
--- src/services/search.ts
+++ src/services/search.ts
@@ -2,10 +2,8 @@ import { getRecommendedGenreType } from "../shared/types"
 import axios from '../shared/axios'
 
 export const getRecommendedGenre = async() : Promise<getRecommendedGenreType> => {
-    const movieGenres =  (await axios.get('/genre/movie/list')).data.genre ;
-    const tvGenres =  ( await axios.get('/genre/tv/list')).data.genre ;
-   
-     console.log(movieGenres , tvGenres )
+    const movieGenres =  (await axios.get('/genre/movie/list')).data.genres ;
+    const tvGenres =  ( await axios.get('/genre/tv/list')).data.genres ;
 
     return {
          movieGenres , tvGenres
