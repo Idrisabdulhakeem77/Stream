dbe17bd74a7b38c31164fd987ad0258f514a2fde
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-01 21:14:09 +0100

    done with bug in recoomended genre component

--- src/components/Home/RecommendedGenre.tsx
+++ src/components/Home/RecommendedGenre.tsx
@@ -1,58 +1,44 @@
-import { useQuery } from "@tanstack/react-query";
-import { FC } from "react";
-import { Link } from "react-router-dom";
-import { getRecommendedGenre } from "../../services/search";
-import { getRecommendedGenreType } from "../../shared/types";
+import { useQuery } from '@tanstack/react-query'
+import {FC} from 'react'
+import {getRecommendedGenre} from '../../services/search'
+import {  getRecommendedGenreType } from "../../shared/types"
 
-const getRandomGenres = (genres: { id: number; name: string }[]) => {
-  // const NUMBER_OF_GENRES = 6;
-  // const randomGenres = [] as { id: number; name: string }[];
-  // [...new Array(NUMBER_OF_GENRES)].forEach((genre) => {
-  //   const randomIndex = Math.floor(Math.random() * genres.length);
-  //   randomGenres.push(genres[randomIndex]);
-  //   genres = genres.filter((item: any) => item.id !== genres[randomIndex].id);
-  // });
 
-  const myChoiceGenresIndex = [5, 2, 13, 14, 6, 7, 4];
-  return myChoiceGenresIndex.map((arrIndex) => genres[arrIndex]);
-};
-
-interface RecommendGenresProps {
-  currentTab: string;
+const getRandomGenres = ( genres : { id : number , name : string}[] ) => {
+   const  randomGenreIndex = [28 , 12 , 16 , 35 , 18 ,99 ,18]
+   return randomGenreIndex.map((arrIndex) => genres[arrIndex])
 }
 
-const RecommendGenres: FC<RecommendGenresProps> = ({ currentTab }) => {
-  const { isLoading, data, isError, error } = useQuery<
-    getRecommendedGenreType,
-    Error
-  >(["genres"], getRecommendedGenre);
 
-  if (isError) return <div>ERROR: {error.message}</div>;
+interface RecommendedGenresProps {
+     currentTab : string
+}
 
-  if (isLoading)
-    return (
-      <div className="mt-36 mb-20 mx-auto h-10 w-10 rounded-full border-[5px] border-dark-lighten border-t-transparent animate-spin"></div>
+
+const RecommendedGenres: FC<RecommendedGenresProps>  = ( { currentTab  }) => {
+   
+
+     const  { data , isError , isLoading , error }  = useQuery<getRecommendedGenreType ,  Error>(["genres"] , getRecommendedGenre) 
+
+      if(isError) return <div> Error </div>    
+      
+        if(isLoading) return <div className="mt-36 mb-20 mx-auto h-10 w-10 rounded-full border-[5px] border-dark-lighten border-t-transparent animate-spin">  </div>
+
+    if(isLoading) return <div>Loading</div>
+       
+    const randomGenres = getRandomGenres(
+      currentTab === "movie" ? data.movieGenres : data.tvGenres
     );
+    
+   
 
-  //  as { id: number; name: string }[]
-  const randomGenres = getRandomGenres(
-    currentTab === "movie" ? data.movieGenres : data.tvGenres
-  );
 
-  return (
-    <ul className="mt-28 flex gap-3 flex-wrap ">
-      {randomGenres.map((genre) => (
-        <li key={genre.id} className="mb-2">
-          <Link
-            to={`/explore?genre=${String(genre.id)}`}
-            className="px-4 py-2 bg-dark-lighten rounded-full hover:brightness-75 transition duration-300"
-          >
-            {genre.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
+      return (
+         <div>
+             Recommended Genre
+         </div>
+      )
+} 
 
-export default RecommendGenres;
+
+export default  RecommendedGenres 
