740f21a4a7f78450878e13834f82f744f4053710
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-01 06:19:25 +0100

    finished the ui for user component

--- src/components/Common/User.tsx
+++ src/components/Common/User.tsx
@@ -4,11 +4,11 @@ import {FC} from 'react'
 
 const User : FC = () => {
      return(
-         <div className='flex gap-2'>
-             <img src="/Images/user.svg"  alt="user" className="w-8 h-8 rounded-full"/>
+         <div className='flex gap-3 mt-2 align-center'>
+             <img src="/Images/user.svg"  alt="user" className="w-10 h-10 rounded-full object-cover"/>
               <div>
                  <h2> Idris Abdulhakeem </h2>
-                 <p> idrisabdulhakeem08@gmail.com</p>
+                 <p className='text-sm'> idrisabdulhakeem08@gmail.com</p>
               </div>
          </div>
      )
