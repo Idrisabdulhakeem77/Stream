60ca9dc6d8a835057f3574643c078a8cb5336a33
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-01 07:12:32 +0100

    added a log statement to view response

--- src/services/search.ts
+++ src/services/search.ts
@@ -4,10 +4,10 @@ import axios from '../shared/axios'
 export const getRecommendedGenre = async() : Promise<getRecommendedGenreType> => {
     const movieGenres =  (await axios.get('/genre/movie/list')).data.genre ;
     const tvGenres =  ( await axios.get('/genre/tv/list')).data.genre ;
-
+   
+     console.log(movieGenres , tvGenres )
 
     return {
          movieGenres , tvGenres
-    }
-     
+    }    
 } 
