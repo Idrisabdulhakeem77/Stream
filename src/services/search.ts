375372a868604318c194e9f74d8b958b9c77b007
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-01 07:11:16 +0100

    bug fix in serach.ts getRecommendedGEnre function

--- src/services/search.ts
+++ src/services/search.ts
@@ -2,8 +2,8 @@ import { getRecommendedGenreType } from "../shared/types"
 import axios from '../shared/axios'
 
 export const getRecommendedGenre = async() : Promise<getRecommendedGenreType> => {
-    const movieGenres = await (await axios.get('/genre/movie/list')).data.genre
-    const tvGenres = await axios.get('/genre/tv/list').data.genre
+    const movieGenres =  (await axios.get('/genre/movie/list')).data.genre ;
+    const tvGenres =  ( await axios.get('/genre/tv/list')).data.genre ;
 
 
     return {
