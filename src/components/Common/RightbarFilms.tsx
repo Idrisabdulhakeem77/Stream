efa561ab46ca86f4b0d625c66cbf20678a6d4868
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-02 18:58:30 +0100

    working on poster image

--- src/components/Common/RightbarFilms.tsx
+++ src/components/Common/RightbarFilms.tsx
@@ -1,6 +1,10 @@
 import {FC} from 'react'
 import {Items} from '../../shared/types'
 import {BsThreeDots} from 'react-icons/bs'
+import Skeleton from './Skeleton';
+import { Link } from 'react-router-dom';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import {resizeImage} from '../../shared/utils'
 
 
 
@@ -16,8 +20,35 @@ interface RightbarFilmsProps {
 const RightbarFilms : FC<RightbarFilmsProps> = (  { films , isLoading , limitNumber , name , className = "" } ) => {
         return (
              <div className={className}> 
-                <p className='capitalize font-bold  justify-between items-center'>{name}</p>
-                <span> <BsThreeDots size={20}/> </span> 
+                <p className='capitalize font-bold  flex justify-between items-center'>
+                     <span> {name} </span>
+                     <BsThreeDots size={20}/>
+                </p>
+
+                <ul className='flex flex-col gap-4'>
+                     { isLoading ? (
+                        new Array(limitNumber).fill('').map( ( _ , index) => (
+                             <li key={index}>
+                                 <Skeleton/>
+                                 </li>
+                        ))
+                     ) :  (
+                         (films?.map((film , index) => (
+                             <li key={film.id}>
+                                 <Link to="">
+                                     <div>
+                                         <LazyLoadImage
+                                             src={ resizeImage(film.backdrop_path , "w150")}
+                                            effect='blur'
+                                            alt='poster'
+                                           />
+                                     </div>
+                                  </Link>
+                             </li>
+                         )))
+                     ) }
+                     
+                </ul>
              </div>
         )
 }
