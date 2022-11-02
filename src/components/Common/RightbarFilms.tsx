96f2573827ea1ce790e864e9e2100fafc7c958df
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-02 07:05:07 +0100

    created the rightbar films

---
+++ src/components/Common/RightbarFilms.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react'
+
+
+
+interface RightbarFilmsProps {}
+
+
+const RightbarFilms : FC<RightbarFilmsProps> = ( {} ) => {
+
+}
+
+
+export default RightbarFilms
+
--- src/components/Home/PopularThisWeek.tsx
+++ src/components/Home/PopularThisWeek.tsx
@@ -1,9 +1,14 @@
 import {FC} from 'react'
+import { useQuery } from '@tanstack/react-query'
 import {getTrendingNow} from  '../../services/home'
+import {  Items } from '../../shared/types'
 
 
 
 const PopularThisWeek : FC = ( ) => {
+    const { data , isLoading , isError , error} = useQuery<Items[] , Error>(["trending"] , getTrendingNow)
+
+    
     return (
          <div>
              Popular
