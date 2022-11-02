2da610ea1316904cfc79a41d303d166d8df99cab
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-02 08:02:29 +0100

    debugging

--- src/components/Common/RightbarFilms.tsx
+++ src/components/Common/RightbarFilms.tsx
@@ -6,12 +6,13 @@ import {Items} from '../../shared/types'
 interface RightbarFilmsProps {
     films : Items[] | undefined ;
     isLoading : boolean ;
+    name : string ;
     limitNumber : number ;
     className?: string
 }
 
 
-const RightbarFilms : FC<RightbarFilmsProps> = (  { films , isLoading} ) => {
+const RightbarFilms : FC<RightbarFilmsProps> = (  { films , isLoading , limitNumber } ) => {
         return (
              <div>
                 RightbarFilms
--- src/components/Home/PopularThisWeek.tsx
+++ src/components/Home/PopularThisWeek.tsx
@@ -9,9 +9,12 @@ import RightbarFilms from '../../components/Common/RightbarFilms'
 const PopularThisWeek : FC = ( ) => {
     const { data , isLoading , isError , error} = useQuery<Items[] , Error>(["trending"] , getTrendingNow)
 
+    if(isError) return <div> Error : { error.message} </div> 
+
     
     return (
          <RightbarFilms
+          name='Popular this week'
           className="mt-4"
             films={data}
             limitNumber={2}
