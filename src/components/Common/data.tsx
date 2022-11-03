bd3a061350b889d0c0ea03ed053f8dfd3fa1259b
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-03 08:33:38 +0100

    finised the data file

--- src/components/Common/data.tsx
+++ src/components/Common/data.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import  {AiOutlineHome} from 'react-icons/ai'
-import {BsFillEyeFill} from 'react-icons/bs'
+import  {AiOutlineHome , AiOutlineHistory , AiOutlineStar}  from 'react-icons/ai'
 import {MdOutlineExplore} from 'react-icons/md'
-
+import { BiSearch, BiUserCircle } from "react-icons/bi";
+import { BsBookmarkHeart , BsCameraVideo } from "react-icons/bs";
+import { HiOutlineLogin } from "react-icons/hi";
+import { RiSlideshow4Line } from "react-icons/ri";
+import {FiAnchor} from "react-icons/fi"
 
 
 export const links = [
@@ -13,65 +16,64 @@ export const links = [
      } ,
 
      {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
+        name : "discover",
+        url : '/discover',
+        icon :  <MdOutlineExplore size={25}/>
     } ,
 
     {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
+        name : "search",
+        url : '/search',
+        icon :  <BiSearch size={25}/>
     } ,
     {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
+        name : "bookmarked",
+        url : '/bookmarked',
+        icon :  <BsBookmarkHeart size={25}/>
     } ,
     {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
+        name : "recent",
+        url : '/recent',
+        icon :  <AiOutlineHistory size={25}/>
     } ,
     {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
+        name : "Top rated",
+        url : '/top_rated',
+        icon :  <AiOutlineStar size={25}/>
     } ,
     {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
+        name : "tv_shows",
+        url : '/tv_shows',
+        icon :  <RiSlideshow4Line size={25}/>
+    } ,
+     {
+         name :"Movies" ,
+         url:'/movies',
+         icon : <BsCameraVideo size={25}/>
+     }  ,
+    {
+        name : "anime",
+        url : '/anime',
+        icon :  <FiAnchor size={25}/>
     } ,
     {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
+        name : "profile",
+        url : '/profile',
+        icon :  <BiUserCircle size={25}/>
     } ,
     {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
+        name : "demo_login",
+        url : '/demo_login',
+        icon :  <HiOutlineLogin size={25}/>
     } ,
+
     {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
+        name : "login",
+        url : '/login',
+        icon :  <HiOutlineLogin size={25}/>
     } ,
-    {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
-    } ,
-    {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
-    } ,
-    {
-        name : "home",
-        url : '/',
-        icon :  AiOutlineHome
-    } ,
+
       
 ]
+
+
