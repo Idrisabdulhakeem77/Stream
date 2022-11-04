207d840b0d1529d016dc12afc843d2609de2f997
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-04 07:17:00 +0100

    finished the useCurrentSearchParams hook

--- src/components/hooks/useCurrentSearchParams.tsx
+++ src/components/hooks/useCurrentSearchParams.tsx
@@ -1,2 +1,17 @@
 import { useSearchParams } from "react-router-dom";
+import { SUPPORTED_QUERY } from "../../shared/constants";
 
+
+export const useCurrentSeaarchParams = () => {
+    const [searchParams ] = useSearchParams()
+
+    const currentSearchParms =  JSON.parse(JSON.stringify(SUPPORTED_QUERY)) as {
+         [key : string] : string[]
+    }
+     
+    searchParams.forEach((value , key) => {
+        currentSearchParms[key].push(value)
+    })
+
+    return [currentSearchParms] as const 
+}
