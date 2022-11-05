99115025d12053fbf467135d8f67e718fc90af4c
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 22:30:25 +0100

    addded the stoe to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 import "swiper/css/bundle"
 import 'react-lazy-load-image-component/src/effects/blur.css';
@@ -17,9 +19,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+       <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
