84f87b6f92566609f10aae2cd0151c52d27bf1b1
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 17:31:21 +0100

    added the getExploreTV to explore.ts

--- src/components/Explore/ExploreResult.tsx
+++ src/components/Explore/ExploreResult.tsx
@@ -5,7 +5,7 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import { ConfigType, ItemsPage } from "../../shared/types";
 import FilmItem from "../Common/FilmItem";
 import Skeleton from "../Common/Skeleton";
-import {getExploreMovie} from '../../services/explore'
+import { getExploreMovie , getExploreTV  } from "../../services/explore";
 
 interface ExploreMovieResultProps {
   pages?: ItemsPage[];
@@ -35,56 +35,73 @@ const ExploreMovieResult: FunctionComponent<ExploreMovieResultProps> = ({
 };
 
 interface ExploreTvResultProps {
-    pages?: ItemsPage[];
-  }
-  
-  const ExploreTvResult: FunctionComponent<ExploreTvResultProps> = ({
-    pages,
-  }) => {
-    return (
-      <ul className="grid grid-cols-sm lg:grid-cols-lg gap-x-8 gap-y-10 pt-2">
-        {pages &&
-          pages.map((page) =>
-            page.results.map((item) => (
-              <li key={item.id}>
-                <FilmItem item={item} />
-              </li>
-            ))
-          )}
-        {!pages &&
-          [...new Array(15)].map((_, index) => (
-            <li key={index}>
-              <Skeleton className="h-0 pb-[160%]" />
-            </li>
-          ))}
-      </ul>
-    );
-  };
-  
-
-
-interface ExploreResultProps {
-   currentTab : string ;
-    config : ConfigType
+  pages?: ItemsPage[];
 }
 
-const ExploreResult: FunctionComponent<ExploreResultProps> = ({ currentTab , config }) => {
-    const {
-        data: movies,
-        error: errorMovies,
-        fetchNextPage: fetchNextPageMovie,
-        hasNextPage: hasNextPageMovie,
-      } = useInfiniteQuery<ItemsPage, Error>(
-        ["explore-result-movie", config],
-        ({ pageParam = 1 }) => getExploreMovie(pageParam, config),
-        {
-          getNextPageParam: (lastPage) =>
-            lastPage.page + 1 <= lastPage.total_pages
-              ? lastPage.page + 1
-              : undefined,
-        }
-      );
- 
+const ExploreTvResult: FunctionComponent<ExploreTvResultProps> = ({
+  pages,
+}) => {
+  return (
+    <ul className="grid grid-cols-sm lg:grid-cols-lg gap-x-8 gap-y-10 pt-2">
+      {pages &&
+        pages.map((page) =>
+          page.results.map((item) => (
+            <li key={item.id}>
+              <FilmItem item={item} />
+            </li>
+          ))
+        )}
+      {!pages &&
+        [...new Array(15)].map((_, index) => (
+          <li key={index}>
+            <Skeleton className="h-0 pb-[160%]" />
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+interface ExploreResultProps {
+  currentTab: string;
+  config: ConfigType;
+}
+
+const ExploreResult: FunctionComponent<ExploreResultProps> = ({
+  currentTab,
+  config,
+}) => {
+  const {
+    data: movies,
+    error: errorMovies,
+    fetchNextPage: fetchNextPageMovie,
+    hasNextPage: hasNextPageMovie,
+  } = useInfiniteQuery<ItemsPage, Error>(
+    ["explore-result-movie", config],
+    ({ pageParam = 1 }) => getExploreMovie(pageParam, config),
+    {
+      getNextPageParam: (lastPage) =>
+        lastPage.page + 1 <= lastPage.total_pages
+          ? lastPage.page + 1
+          : undefined,
+    }
+  );
+
+  const {
+    data: tvs,
+    error: errorTvs,
+    fetchNextPage: fetchNextPageTv,
+    hasNextPage: hasNextPageTv,
+  } = useInfiniteQuery<ItemsPage, Error>(
+    ["explore-result-tv", config],
+    ({ pageParam = 1, queryKey }) =>
+      getExploreTV(pageParam, queryKey[1] as { [key: string]: string }),
+    {
+      getNextPageParam: (lastPage) =>
+        lastPage.page + 1 <= lastPage.total_pages
+          ? lastPage.page + 1
+          : undefined,
+    }
+  );
 
   return <div>Explore Results</div>;
 };
--- src/services/explore.ts
+++ src/services/explore.ts
@@ -30,3 +30,29 @@ export const getExploreMovie: (
   };
 
 
+
+  export const getExploreTV: (
+    page: number,
+    config?: ConfigType
+  ) => Promise<ItemsPage> = async (page, config = {}) => {
+    const data = (
+      await axios.get("/discover/movie", {
+        params: {
+          ...config,
+          page,
+        },
+      })
+    ).data;
+  
+    const adjustedItems = data.results
+      .filter((item: Items) => item.poster_path)
+      .map((item: any) => ({
+        ...item,
+        media_type: "movie",
+      }));
+  
+    return {
+      ...data,
+      results: adjustedItems,
+    };
+  };
