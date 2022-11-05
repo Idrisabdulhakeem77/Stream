d841dcc7cadcb506c83f2141eb0423cadecf3d9d
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 08:30:30 +0100

    added the tabs componet

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -14,6 +14,9 @@ interface ExploreProps {}
 const Explore: FC<ExploreProps> = () => {
   const [showScrollBtn, setShowStrollBtn] = useState(true);
   const [isSiderBarActive, setIsSidebarActive] = useState(false);
+  const [currentTab, setCurrentTab] = useState(
+    localStorage.getItem("currentTab")
+  );
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -72,6 +75,48 @@ const Explore: FC<ExploreProps> = () => {
               </div>
             </div>
           )}
+
+<div className="flex justify-between items-center md:items-start">
+            {/* Button Div */}
+            <div className="inline-flex  gap-[40px]  relative pb-3  mt-2 md:mt-4  text-gray-600 ">
+              <button
+                onClick={() => {
+                  setCurrentTab("movie");
+                  localStorage.setItem("currentTab", "movie");
+                }}
+                className={`${
+                  currentTab === "movie" &&
+                  "font-medium  transition duration-200 hover:text-black   after:absolute after:h-[3px] after:w-5 after:bottom-0 after:left-[10%] after:bg-black"
+                }`}
+              >
+                Movie
+              </button>
+              <button
+                onClick={() => {
+                  setCurrentTab("tv");
+                  localStorage.setItem("currentTab", "tv");
+                }}
+                className={`${
+                  currentTab === "tv" &&
+                  "font-medium  transition duration-200 hover:text-black   after:absolute after:h-[3px] after:w-5 after:bottom-0 after:left-[45%] after:bg-black"
+                }`}
+              >
+                Tv Shows
+              </button>
+              <button
+                onClick={() => {
+                  setCurrentTab("anime");
+                  localStorage.setItem("currentTab", "anime");
+                }}
+                className={`${
+                  currentTab === "anime" &&
+                  "font-medium  transition duration-200 hover:text-black   after:absolute after:h-[3px] after:w-5 after:bottom-0 after:left-[80%] after:bg-black"
+                }`}
+              >
+                Anime
+              </button>
+            </div>
+            </div>
         </div>
       </div>
 
