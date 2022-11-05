489d5c409b90e81425c8c5bc759d61f0cd8aa868
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 23:06:30 +0100

    finished creating userSlice

--- src/store/slice/userSlice.ts
+++ src/store/slice/userSlice.ts
@@ -1,2 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { User } from "../../shared/types";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
-import {User} from '../../shared/types'
+interface AuthUser {
+  user: User | null;
+}
+
+const initialState = {
+  user: null,
+} as AuthUser;
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setCurrentUser : (state , action : PayloadAction<User | null>) => {
+          state.user = action.payload
+    }
+  },
+});
+
+
+
+export const { setCurrentUser } = userSlice.actions;
+
+export default userSlice.reducer;
