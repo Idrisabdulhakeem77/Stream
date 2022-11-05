4cd3b5ce56a03d735ec6a8d27dc8bced3e7c331b
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 17:14:04 +0100

    added styles to the ul tag

--- src/components/Explore/ExploreResult.tsx
+++ src/components/Explore/ExploreResult.tsx
@@ -1,21 +1,21 @@
-import { FunctionComponent , useState } from "react";
-import {useInfiniteQuery} from '@tanstack/react-query'
-import  InfiniteScroll from 'react-infinite-scroll-component'
-import   {LazyLoadImage} from 'react-lazy-load-image-component'
-import {ConfigType , ItemsPage} from '../../shared/types'
-import FilmItem from '../Common/FilmItem'
-import Skeleton from '../Common/Skeleton'
-
+import { FunctionComponent, useState } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { ConfigType, ItemsPage } from "../../shared/types";
+import FilmItem from "../Common/FilmItem";
+import Skeleton from "../Common/Skeleton";
 
 interface ExploreMovieResultProps {
-     pages?: ItemsPage[]
-} 
+  pages?: ItemsPage[];
+}
 
-
-const ExploreMovieResult : FunctionComponent<ExploreMovieResultProps>  = ({ pages} ) => {
-     return (
-         <ul>
-              {pages &&
+const ExploreMovieResult: FunctionComponent<ExploreMovieResultProps> = ({
+  pages,
+}) => {
+  return (
+    <ul className="grid grid-cols-sm lg:grid-cols-lg gap-x-8 gap-y-10 pt-2">
+      {pages &&
         pages.map((page) =>
           page.results.map((item) => (
             <li key={item.id}>
@@ -29,25 +29,16 @@ const ExploreMovieResult : FunctionComponent<ExploreMovieResultProps>  = ({ page
             <Skeleton className="h-0 pb-[160%]" />
           </li>
         ))}
-         </ul>
-     )
-     
+    </ul>
+  );
+};
+
+interface ExploreResultProps {
+  pages?: ItemsPage[];
 }
 
+const ExploreResult: FunctionComponent<ExploreResultProps> = ({ pages }) => {
+  return <div>Explore Results</div>;
+};
 
-
-interface ExploreResultProps { 
-     pages?:ItemsPage[]
-}
-
-
-const ExploreResult : FunctionComponent<ExploreResultProps> = ( {pages} ) => {
-      return (
-          <div>
-             Explore Results
-          </div>
-      )
-} 
-
-
-export default ExploreResult
+export default ExploreResult;
