640b7efcb9776374d0b38f0dedba7611164adb96
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 17:12:58 +0100

    created a exploremovieResult functional comp

--- src/components/Explore/ExploreResult.tsx
+++ src/components/Explore/ExploreResult.tsx
@@ -7,6 +7,35 @@ import FilmItem from '../Common/FilmItem'
 import Skeleton from '../Common/Skeleton'
 
 
+interface ExploreMovieResultProps {
+     pages?: ItemsPage[]
+} 
+
+
+const ExploreMovieResult : FunctionComponent<ExploreMovieResultProps>  = ({ pages} ) => {
+     return (
+         <ul>
+              {pages &&
+        pages.map((page) =>
+          page.results.map((item) => (
+            <li key={item.id}>
+              <FilmItem item={item} />
+            </li>
+          ))
+        )}
+      {!pages &&
+        [...new Array(15)].map((_, index) => (
+          <li key={index}>
+            <Skeleton className="h-0 pb-[160%]" />
+          </li>
+        ))}
+         </ul>
+     )
+     
+}
+
+
+
 interface ExploreResultProps { 
      pages?:ItemsPage[]
 }
