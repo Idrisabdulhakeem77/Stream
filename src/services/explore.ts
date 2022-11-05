8b0ae8f4b01510988d1dd839c40f9500bf89090f
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 09:08:39 +0100

    added a getExploreMovie

---
+++ src/services/explore.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+import { ConfigType, Items, ItemsPage } from "../shared/types";
+
+
+export const getExploreMovie: (
+    page: number,
+    config?: ConfigType
+  ) => Promise<ItemsPage> = async (page, config = {}) => {
+    const data = (
+      await axios.get("/discover/movie", {
+        params: {
+          ...config,
+          page,
+        },
+      })
+    ).data;
+  
+    const adjustedItems = data.results
+      .filter((item: Items) => item.poster_path)
+      .map((item: any) => ({
+        ...item,
+        media_type: "movie",
+      }));
+  
+    return {
+      ...data,
+      results: adjustedItems,
+    };
+  };
