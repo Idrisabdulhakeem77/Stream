2b1d1e28c0f9fd8ad74776addfd683594ea6f5e1
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 22:59:39 +0100

    added content to the user type

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,50 +1,50 @@
 export interface Items {
-    poster_path: string;
-    overview: string;
-    genre_ids: number[];
-    id: number;
-    original_language: string;
-    backdrop_path: string;
-    popularity: number;
-    vote_count: number;
-    vote_average: number;
-    media_type: "movie" | "tv" | "person";
-    release_date?: string;
-    original_title?: string;
-    title?: string;
-    adult?: boolean;
-    video?: boolean;
-    first_air_date?: string;
-    original_name?: string;
-    origin_country?: string[];
-    name?: string;
-    profile_path?: string;
+  poster_path: string;
+  overview: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  backdrop_path: string;
+  popularity: number;
+  vote_count: number;
+  vote_average: number;
+  media_type: "movie" | "tv" | "person";
+  release_date?: string;
+  original_title?: string;
+  title?: string;
+  adult?: boolean;
+  video?: boolean;
+  first_air_date?: string;
+  original_name?: string;
+  origin_country?: string[];
+  name?: string;
+  profile_path?: string;
 }
 
-
 export interface HomeFilms {
-     [key : string] : Items[];
+  [key: string]: Items[];
 }
 
 export interface getRecommendedGenreType {
-     movieGenres : { id : number , name : string}[];
-     tvGenres : { id : number , name : string}[];
+  movieGenres: { id: number; name: string }[];
+  tvGenres: { id: number; name: string }[];
 }
 
-
 export interface ItemsPage {
-    page: number;
-    results: Items[];
-    total_results: number;
-    total_pages: number;
-  }
+  page: number;
+  results: Items[];
+  total_results: number;
+  total_pages: number;
+}
 
+export interface ConfigType {
+  [key: string]: string | number;
+}
 
-  export interface ConfigType { 
-     [key: string]: string | number;
-  }
-
-
- export interface User {
-     
- } 
+export interface User {
+  displayName: string | null;
+  email: string | null;
+  emailVerified: boolean;
+  photoURL: string | null;
+  uid: string;
+}
