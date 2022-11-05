9a3311104f18f5cbd02821a7893648cab9bc8ca0
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 23:07:44 +0100

    added a user to the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import  userReducer from './slice/userSlice'
 
 
 
 export const store = configureStore ( {
      reducer : {
-         
+        user : userReducer
      }
 })
 
