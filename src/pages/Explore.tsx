3d93a45ce767d76bffb9a45603e89309e8eba036
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 08:12:23 +0100

    added the title and hamburger menu

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -2,21 +2,27 @@ import { FC, useState, useEffect } from "react";
 import Title from "../components/Common/Title";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
 import MiniSidebar from "../components/Common/MiniSidebar";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 import SortBy from "../components/Explore/SortBy";
 
 interface ExploreProps {}
 
 const Explore: FC<ExploreProps> = () => {
   const [showScrollBtn, setShowStrollBtn] = useState(true);
-  const scrollTo = () => {
-    console.log("scrolling");
+  const [isSiderBarActive, setIsSidebarActive] = useState(false);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
   return (
     <>
       <Title value="Explore" />
       {showScrollBtn && (
         <button
-          onClick={scrollTo}
+          onClick={scrollToTop}
           className={`fixed bottom-[30px] right-[30px] z-10 transition duration-500 ${
             showScrollBtn ? "opacity-100" : "opacity-0"
           }`}
@@ -27,6 +33,18 @@ const Explore: FC<ExploreProps> = () => {
           />
         </button>
       )}
+
+      <div className="flex justify-between items-center my-4 px-4 md:hidden">
+        <Link to="/">
+          <div className="uppercase font-medium text-lg tracking-widest">
+            {" "}
+            AnimeStream{" "}
+          </div>
+        </Link>
+        <button onClick={() => setIsSidebarActive((prevState) => !prevState)}>
+          <FaBars size={25} />
+        </button>
+      </div>
 
       <SortBy />
     </>
