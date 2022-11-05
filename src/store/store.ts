f058fe326b8638e3a6736d0fb9fbac074c8d9eb3
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-05 22:28:55 +0100

    initialize the redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,13 @@
-import
+import { configureStore } from "@reduxjs/toolkit";
+
+
+
+export const store = configureStore ( {
+     reducer : {
+         
+     }
+})
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
