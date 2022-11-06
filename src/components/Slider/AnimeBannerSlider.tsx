249d5859376309a08c5feefa41849f7917641e7d
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-06 07:26:07 +0100

    finished basic laout for anime banner slider

--- src/components/Slider/AnimeBannerSlider.tsx
+++ src/components/Slider/AnimeBannerSlider.tsx
@@ -6,3 +6,19 @@ import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { useCurrentViewPort } from '../hooks/useCurrentViewPort'
 
 
+interface AnimeBannerSliderProps {
+     animes : Animes[] | undefined ;
+
+}
+
+
+const AnimeBannerSlider : FC<AnimeBannerSliderProps>  = () => {
+     return (
+         <div>
+             Anime
+         </div>
+     )
+}
+
+
+export default AnimeBannerSlider
