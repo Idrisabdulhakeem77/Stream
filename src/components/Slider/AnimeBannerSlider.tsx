fc9cf65bdb4a5a9dafece31388315aeb897f0e47
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-06 16:21:59 +0100

    bug fix in anime.tsx

--- src/components/Slider/AnimeBannerSlider.tsx
+++ src/components/Slider/AnimeBannerSlider.tsx
@@ -16,26 +16,26 @@ const AnimeBannerSlider: FC<AnimeBannerSliderProps> = ({ animes }) => {
   return (
     <>
       <div className="mt-4 relative h-4 md:pb-[45%] pb-[35%]  tw-banner-slider mb-4  ">
-      <Swiper
-        navigation={true}
-        effect={"flip"}
-        autoplay={{ delay: 6000, disableOnInteraction: false }}
-        grabCursor={true}
-        modules={[Navigation, Autoplay, EffectFlip]}
-        slidesPerView={1}
-        className="!absolute !top-0 !left-0 w-[450px] h-[400px] !rounded-lg place-content-center place-items-center"
-      >
-        {(animes as Animes[]).map((anime, index) => {
-             const image = anime.images.jpg.large_image_url
+        <Swiper
+          navigation={true}
+          effect={"flip"}
+          autoplay={{ delay: 6000, disableOnInteraction: false }}
+          grabCursor={true}
+          modules={[Navigation, Autoplay, EffectFlip]}
+          slidesPerView={1}
+          className="!absolute !top-0 !left-0 w-[450px] h-[400px] !rounded-lg place-content-center place-items-center"
+        >
+          {(animes as Animes[]).map((anime, index) => {
+            const image = anime.images.jpg.large_image_url;
             return (
               <SwiperSlide>
                 <Link to="/">
-                   <LazyLoadImage  src={`${image}`}   />
+                  <LazyLoadImage src={`${image}`} />
                 </Link>
               </SwiperSlide>
             );
-        })}
-      </Swiper>
+          })}
+        </Swiper>
       </div>
     </>
   );
