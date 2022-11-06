fa93be5108f61bfea3d337eecf2d0ddcae09fb09
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-06 15:39:53 +0100

    deleted some unused code i anime comp

--- src/components/Home/Animes.tsx
+++ src/components/Home/Animes.tsx
@@ -18,26 +18,7 @@ const Anime : FC<AnimeProps> = ( { data  , isSectionLoading }) => {
           <AnimeBannerSlider animes={data}/>
 
            <ul className='flex flec-col mt-12'>
-           {isSectionLoading ? (
-          <>
-            {new Array(2).fill("").map((_, index) => (
-              <li key={index}>
-                <Skeleton className="mb-3 max-w-[10%] h-8 rounded-md" />
-                <SectionSlider films={undefined} />
-              </li>
-            ))}
-          </>
-        ) : (
-          Object.entries(data as Animes[])
-            .map((section, index) =>  {
-                console.log(section)
-                return (
-                   <div>
-                       Section
-                   </div>
-                )
-            })
-        )}
+               
            </ul>
          </>
      )
