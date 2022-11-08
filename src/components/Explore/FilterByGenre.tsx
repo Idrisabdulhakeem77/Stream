670a8177138bb7af28eacb348d463c3eaae61926
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-08 21:00:22 +0100

    working on filterby genre

--- src/components/Explore/FilterByGenre.tsx
+++ src/components/Explore/FilterByGenre.tsx
@@ -1,18 +1,25 @@
-import {FunctionComponent} from 'react'
-
-
+import { FunctionComponent } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import {useQuery} from '@tanstack/react-query'
+import {getRecommendedGenreType} from '../../shared/types'
+import { getRecommendedGenre} from '../../services/search'
 interface FilterByGenreProps {
-     currentTab  : string | null
+  currentTab: string | null;
 }
 
+const FilterByGenre: FunctionComponent<FilterByGenreProps> = ({
+  currentTab,
+}) => {
+  const [genres] = useAutoAnimate();
+  const { data , isLoading , error , isError} = useQuery<getRecommendedGenreType ,Error>(["genres"] , getRecommendedGenre)
+  return (
+    <div
+      //@ts-ignore
+      ref={genres}
+    >
+      FilterByGenre
+    </div>
+  );
+};
 
-const FilterByGenre : FunctionComponent<FilterByGenreProps>  = ( { currentTab}) => {
-     return (
-         <div>
-             FilterByGenre
-         </div>
-     )
-}
-
-
-export default FilterByGenre
+export default FilterByGenre;
