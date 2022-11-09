965c4808ead4878cebb7657231509ccf0a4ed62d
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-09 08:02:29 +0100

    created the handle filterDate function

--- src/components/Explore/FilterByDate.tsx
+++ src/components/Explore/FilterByDate.tsx
@@ -1,8 +1,27 @@
-import { FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useCurrentSeaarchParams } from '../hooks/useCurrentSearchParams'
 
 interface FilterByDateProps {}
 
 const FilterByDate: FunctionComponent<FilterByDateProps> = () => {
+  const [searchParams , setSearchParams] = useSearchParams()
+  const [currentSearchParms]  = useCurrentSeaarchParams()
+
+  const handleFilterDate =  ( e : React.ChangeEvent<HTMLInputElement>) => {
+     if(e.target.name === "from") {
+         setSearchParams({ 
+             ...currentSearchParms ,
+             from  : e.target.value
+         } )
+     }  else {
+         setSearchParams({
+             ...currentSearchParms,
+            to : e.target.value
+         })
+     }
+  }
+
   return <div>FilterByDate</div>;
 };
 
