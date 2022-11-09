e49448c4fe89692913bb64f26c776cffab64ed16
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-09 20:51:43 +0100

    c

---
+++ src/components/Auth/ModalNotfication.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+
+interface ModalNotificationProps {} 
+
+
+const ModalNotification : FunctionComponent<ModalNotificationProps> = ( ) => {
+     return (
+         
+     )
+}
+
+export default ModalNotification
