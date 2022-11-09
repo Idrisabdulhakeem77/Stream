1a24b0c1bb29bc23949f025ef51d3e0c9fc33cba
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-09 06:13:24 +0100

    added the updateMinRangeBar

--- src/components/Explore/FilterByRating.tsx
+++ src/components/Explore/FilterByRating.tsx
@@ -1,17 +1,40 @@
-import { FunctionComponent } from "react";
-
+import React, { FunctionComponent, useState, useEffect, useRef } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
+import { useCurrentSeaarchParams } from "../hooks/useCurrentSearchParams";
 
 interface FilterByRatingProps {}
 
+const MAX_RUNTIME = 200;
+const gap = 20;
 
-const FilterByRating : FunctionComponent<FilterByRatingProps> = () => {\
-    return (
-        <div>
-             Filter By rating
-        </div>
-    )
-     
-}
+const FilterByRating: FunctionComponent<FilterByRatingProps> = () => {
+  const sliderRangeRef = useRef<HTMLDivElement>(null!);
+  const location = useLocation();
+
+  const [minRuntime, setMinRuntime] = useState(0);
+  const [maxRuntime, setMaxRuntime] = useState(200);
+
+  const timeoutRef = useRef<any>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [currentSearchparams] = useCurrentSeaarchParams();
+
+  useEffect(() => {
+    updateMinRangeBar(Number(searchParams.get("minRuntime")) ?? 0);
+    updateMaxRangeBar(Number(searchParams.get("maxRuntime")) || 200);
+
+    // eslint-disable-next-line
+  }, [location.search]);
 
 
-export default FilterByRating
+  const updateMinRangeBar = (value: number) => {
+    setMinRuntime(value);
+    const leftOffet = (value / MAX_RUNTIME) * 100;
+    sliderRangeRef.current.style.left = leftOffet + "%";
+  };
+
+  
+
+  return <div>Filter By rating</div>;
+};
+
+export default FilterByRating;
