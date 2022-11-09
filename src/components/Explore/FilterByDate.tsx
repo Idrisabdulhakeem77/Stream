08f81e6f461e093eb1d1e533906e156264f2ddc1
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-09 08:08:12 +0100

    added the label for the from

--- src/components/Explore/FilterByDate.tsx
+++ src/components/Explore/FilterByDate.tsx
@@ -1,28 +1,41 @@
 import React, { FunctionComponent } from "react";
 import { useSearchParams } from "react-router-dom";
-import { useCurrentSeaarchParams } from '../hooks/useCurrentSearchParams'
+import { useCurrentSeaarchParams } from "../hooks/useCurrentSearchParams";
 
 interface FilterByDateProps {}
 
 const FilterByDate: FunctionComponent<FilterByDateProps> = () => {
-  const [searchParams , setSearchParams] = useSearchParams()
-  const [currentSearchParms]  = useCurrentSeaarchParams()
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [currentSearchParms] = useCurrentSeaarchParams();
 
-  const handleFilterDate =  ( e : React.ChangeEvent<HTMLInputElement>) => {
-     if(e.target.name === "from") {
-         setSearchParams({ 
-             ...currentSearchParms ,
-             from  : e.target.value
-         } )
-     }  else {
-         setSearchParams({
-             ...currentSearchParms,
-            to : e.target.value
-         })
-     }
-  }
+  const handleFilterDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "from") {
+      setSearchParams({
+        ...currentSearchParms,
+        from: e.target.value,
+      });
+    } else {
+      setSearchParams({
+        ...currentSearchParms,
+        to: e.target.value,
+      });
+    }
+  };
 
-  return <div>FilterByDate</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-between items-center">
+        <label htmlFor="from"> From</label>
+        <input
+          type="date"
+          name="from"
+          id="from"
+          onChange={(e) => handleFilterDate(e)}
+          value={searchParams.get("from") || "2000-12-13"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default FilterByDate;
