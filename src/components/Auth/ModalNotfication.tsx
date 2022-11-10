314ee0b1a9d714b1a85bd29f4cb40fa42cd85c37
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-10 21:18:56 +0100

    added custom style to index.css

--- src/components/Auth/ModalNotfication.tsx
+++ src/components/Auth/ModalNotfication.tsx
@@ -1,48 +1,58 @@
-import { FunctionComponent , useEffect, useState } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
-
 interface ModalNotificationProps {
-    type : string ,
-    setError ?: any ,
-    message : string
-} 
-
-const TIMEOUT_AUTO_CLOSE_ERROR = 5
-const TIMEOUT_AUTO_CLOSE_SUCCESS = 2
-
-
-const ModalNotification : FunctionComponent<ModalNotificationProps> = (  { type , message , setError} ) => {
-   const [timeLeft , setTimeLeft] = useState( type === "success" ? TIMEOUT_AUTO_CLOSE_SUCCESS : TIMEOUT_AUTO_CLOSE_ERROR)
-   const navigate = useNavigate()
-   const [searchParams] = useSearchParams()
-
-   const isCloseModalAutomatically = timeLeft === 0; 
-
-   useEffect(() => {
-     if(isCloseModalAutomatically) {
-         navigate(searchParams.get('redirect') || "/")
-     }  else {
-         setError('')
-     }
-       //eslint-disable-next-line
-   } , [isCloseModalAutomatically])
-
-   useEffect(() => {
-      const timeout = setInterval(() => {
-          setTimeLeft(prev => prev - 1)
-      } , 1000)
-
-      return () => clearInterval(timeout)
-   } , []) 
- 
- 
-   
-     return ( 
-         <div>
-            
-            </div>
-    )
+  type: string;
+  setError?: any;
+  message: string;
 }
 
-export default ModalNotification
+const TIMEOUT_AUTO_CLOSE_ERROR = 5;
+const TIMEOUT_AUTO_CLOSE_SUCCESS = 2;
+
+const ModalNotification: FunctionComponent<ModalNotificationProps> = ({
+  type,
+  message,
+  setError,
+}) => {
+  const [timeLeft, setTimeLeft] = useState(
+    type === "success" ? TIMEOUT_AUTO_CLOSE_SUCCESS : TIMEOUT_AUTO_CLOSE_ERROR
+  );
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const isCloseModalAutomatically = timeLeft === 0;
+
+  useEffect(() => {
+    if (isCloseModalAutomatically) {
+      navigate(searchParams.get("redirect") || "/");
+    } else {
+      setError("");
+    }
+    //eslint-disable-next-line
+  }, [isCloseModalAutomatically]);
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timeout);
+  }, []);
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: `${
+            type === "success" ? "url(/Images/success)" : "url(/Images/fail)"
+          } `
+
+        }}
+        className="bg-cover bg-no-repeat bg-center min-h-[450px] w-full max-w-[350px] fixed rounded-xl z-20 "
+      ></div>
+    </>
+  );
+};
+
+export default ModalNotification;
