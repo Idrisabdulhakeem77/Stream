4aa33f01e0b0741710e1fa28e6daa01d08e6a28c
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-12 14:00:15 +0100

    added some code to sidebar

--- src/components/Common/Sidebar.tsx
+++ src/components/Common/Sidebar.tsx
@@ -3,14 +3,14 @@ import { AiOutlineHome, AiOutlineHistory, AiOutlineStar } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { MdOutlineExplore } from "react-icons/md";
 import { BiSearch, BiUserCircle } from "react-icons/bi";
-import { BsBookmarkHeart , BsCameraVideo } from "react-icons/bs";
+import { BsBookmarkHeart, BsCameraVideo } from "react-icons/bs";
 import { HiOutlineLogin } from "react-icons/hi";
 import { useCurrentViewPort } from "../hooks/useCurrentViewPort";
 import { BsFillEyeFill } from "react-icons/bs";
 import { RiSlideshow4Line } from "react-icons/ri";
-import {FiAnchor} from "react-icons/fi"
-import {FaTimes} from "react-icons/fa"
-
+import { FiAnchor } from "react-icons/fi";
+import { FaTimes } from "react-icons/fa";
+import { useAppSelector } from "../../store/hooks";
 
 interface SidebarProps {
   isSidebarOpen: boolean;
@@ -19,13 +19,17 @@ interface SidebarProps {
 
 const Sidebar: FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const { isMobile } = useCurrentViewPort();
-
+  const currentUser = useAppSelector((state) => state.user.user);
   const closeSideBar = () => {
-    setIsSidebarOpen(false)
-  }
+    setIsSidebarOpen(false);
+  };
   return (
     <>
-       <div className={`shrink-0 h-full md:max-w-[260px] w-[70vw] bg-slate-300  fixed  -translate-x-full transition duration-300 ${isSidebarOpen && "translate-x-0"} top-0 shadow-md md:sticky md:translate-x-0 md:bg-transparent md:shadow-none   z-50 ` }>
+      <div
+        className={`shrink-0 h-full md:max-w-[260px] w-[70vw] bg-slate-300  fixed  -translate-x-full transition duration-300 ${
+          isSidebarOpen && "translate-x-0"
+        } top-0 shadow-md md:sticky md:translate-x-0 md:bg-transparent md:shadow-none   z-50 `}
+      >
         {!isMobile && (
           <Link
             to="/"
@@ -38,10 +42,10 @@ const Sidebar: FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
             </h1>
           </Link>
         )}
-        <div  className="md:hidden absolute top-2 right-4 text-red-dark hover:text-red-400 bg-transparent transition ">
-           <button onClick={closeSideBar}>
-             <FaTimes size={25}/>
-           </button>
+        <div className="md:hidden absolute top-2 right-4 text-red-dark hover:text-red-400 bg-transparent transition ">
+          <button onClick={closeSideBar}>
+            <FaTimes size={25} />
+          </button>
         </div>
         {/* <div className="hidden   text-black text-xs text-center sm:hidden">MENU</div> */}
         <div className="flex flex-col gap-4 mt-4 ml-4 px-4">
@@ -87,12 +91,11 @@ const Sidebar: FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
             <p>Top Rated</p>
           </button>
         </div>
-{/* 
+        {/* 
         <div className="text-black text-lg font-medium mt-4 text-center">
           Categories
         </div> */}
         <div className="flex flex-col gap-4 mt-4 ml-4 px-4">
-
           <button className="flex gap-3 items-center hover:text-gray-700 transition duration-300">
             <RiSlideshow4Line size={25} />
             <p>Tv Shows</p>
@@ -121,10 +124,17 @@ const Sidebar: FC<SidebarProps> = ({ isSidebarOpen, setIsSidebarOpen }) => {
             <p>Demo Login</p>
           </button>
 
-          <button className="flex gap-3 items-center ">
-            <HiOutlineLogin size={25} />
-            <p> Login</p>
-          </button>
+          {currentUser ? (
+            <button className="flex gap-3 items-center ">
+              <HiOutlineLogin size={25} />
+              <p> Logout</p>
+            </button>
+          ) : (
+            <button className="flex gap-3 items-center ">
+              <HiOutlineLogin size={25} />
+              <p> Login</p>
+            </button>
+          )}
         </div>
       </div>
     </>
