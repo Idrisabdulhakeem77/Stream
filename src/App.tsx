0bf38cd0a8181c44b34eb2611ffd7f9dc0710404
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-12 15:28:16 +0100

    set user to incoming user data from firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { Route  , Routes, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 
-import Explore from './pages/Explore';
-import Home from './pages/Home';
-import Auth from './pages/Auth';
-import { useAppDispatch } from './store/hooks';
-import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from './shared/firebase';
-import {setCurrentUser} from './store/slice/userSlice'
+import Explore from "./pages/Explore";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import { useAppDispatch } from "./store/hooks";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./shared/firebase";
+import { setCurrentUser } from "./store/slice/userSlice";
+import userEvent from "@testing-library/user-event";
+import { onSnapshot, doc } from "firebase/firestore";
+import { db } from "./shared/firebase";
 
 function App() {
-   
-  const [isSignedIn , setIsSignedIn] = useState( Number(localStorage.getItem("isSignedIn")) ? true : false)
-  const dispatch = useAppDispatch()
-  const location = useLocation()
+  const [isSignedIn, setIsSignedIn] = useState(
+    Number(localStorage.getItem("isSignedIn")) ? true : false
+  );
+  const dispatch = useAppDispatch();
+  const location = useLocation();
 
   useEffect(() => {
-     onAuthStateChanged(auth , ( user) => {
-         if(!user) {
-           dispatch(setCurrentUser(null))
-           setIsSignedIn(false)
-            localStorage.setItem("isSignedIn" , "0")
-            return
-         }
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        dispatch(setCurrentUser(null));
+        setIsSignedIn(false);
+        localStorage.setItem("isSignedIn", "0");
+        return;
+      }
 
-         
-         setIsSignedIn(true)
-         localStorage.setItem("isSignedIn" , "1")
-     })
+      setIsSignedIn(true);
+      localStorage.setItem("isSignedIn", "1");
 
-   } , [])
+      if (user.providerData[0].providerId === "google.com") {
+        onSnapshot(doc(db, "users", user.uid), (doc) => {
+          console.log(doc.data());
+          dispatch(
+            setCurrentUser({
+              email: user.email,
+              displayName: user.displayName,
+              emailVerified: doc.data()?.emailVerified,
+              photoURL: doc.data()?.photoURL,
+              uid: user.uid,
+            })
+          );
+        });
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="App">
-         <Routes>
-             <Route path='/' element={<Home/>}/> 
-             <Route path='explore' element={<Explore/>} />
-             <Route path="auth" element={<Auth />} />
-         </Routes>
-         
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="explore" element={<Explore />} />
+        <Route path="auth" element={<Auth />} />
+      </Routes>
     </div>
   );
 }
