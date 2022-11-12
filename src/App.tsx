d947ce5b21ccd3ed545ceb683425c7b1fa70fb46
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-12 15:05:17 +0100

    finished checking for user

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route  , Routes, useLocation } from 'react-router-dom';
 
 import Explore from './pages/Explore';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
 import { useAppDispatch } from './store/hooks';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './shared/firebase';
+import {setCurrentUser} from './store/slice/userSlice'
 
 function App() {
    
-  const [isSignedIn , setIsSignedIn] = useState( Number(localStorage.getItem("isSIgnedIn") ? true : false))
+  const [isSignedIn , setIsSignedIn] = useState( Number(localStorage.getItem("isSignedIn")) ? true : false)
   const dispatch = useAppDispatch()
   const location = useLocation()
 
-  usEff
+  useEffect(() => {
+     onAuthStateChanged(auth , ( user) => {
+         if(!user) {
+           dispatch(setCurrentUser(null))
+           setIsSignedIn(false)
+            localStorage.setItem("isSignedIn" , "0")
+            return
+         }
+
+         
+         setIsSignedIn(true)
+         localStorage.setItem("isSignedIn" , "1")
+     })
+
+   } , [])
   return (
     <div className="App">
          <Routes>
