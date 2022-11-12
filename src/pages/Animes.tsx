698a199b12723737786ccbaee9adca1878be8607
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-12 16:43:46 +0100

    created the anime page

---
+++ src/pages/Animes.tsx
@@ -0,0 +1,17 @@
+import {FC } from 'react'
+
+
+interface AnimeProps {} 
+
+
+const Anime : FC<AnimeProps>  = ( ) => {
+    return (
+         <div>
+             Anime 
+         </div>
+    )
+     
+} 
+
+
+export default Anime 
