fe434e30a9c540abc5cb97040b354a1c7deee3e1
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-13 21:30:28 +0100

    added the scroll up button with smooth scroll

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,15 +1,34 @@
-import { FC , useState, useEffect } from "react";
+import { FC, useState, useEffect } from "react";
+import { useCurrentViewPort } from "../components/hooks/useCurrentViewPort";
 
 interface MoviesProps {}
 
+const Movies: FC<MoviesProps> = () => {
+  const [showScrollBtn, setShowStrollBtn] = useState(false);
+  const { isMobile } = useCurrentViewPort();
+  const [isSiderBarActive, setIsSidebarActive] = useState(false);
+  useEffect(() => {
+    const checkIfScrollbuttonShowUp = () => {
+      const scrollOffset = document.documentElement.scrollTop;
 
-const Movies : FC<MoviesProps> = () => {
-     return (
-         <div>
-             Movies
-         </div>
-     )
-}
+      if (scrollOffset > 1000) {
+        setShowStrollBtn(true);
+      } else {
+        setShowStrollBtn(false);
+      }
+    };
+    window.addEventListener("scroll", checkIfScrollbuttonShowUp);
+    return () =>
+      window.removeEventListener("scroll", checkIfScrollbuttonShowUp);
+  }, []);
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return <div>Movies</div>;
+};
 
-export default Movies
+export default Movies;
