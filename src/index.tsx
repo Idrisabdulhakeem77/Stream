ccb33f2a9d4efc60a02637d4342d294791713552
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-14 08:57:02 +0100

    added the toastify-css to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
@@ -12,6 +13,7 @@ import "swiper/css/bundle";
 import "swiper/css";
 import "swiper/css/navigation";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient();
 
