a2897bc85f2dbdf1a838a587fdb263dabf2bb942
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-15 20:36:14 +0100

    removed facebook from signin method

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ function App() {
 
       if (user.providerData[0].providerId === "google.com") {
         onSnapshot(doc(db, "users", user.uid), (doc) => {
+           console.log(doc.data()?.photoURL)
           dispatch(
             setCurrentUser({
               email: user.email,
--- src/components/Auth/signInWithProvider.ts
+++ src/components/Auth/signInWithProvider.ts
@@ -1,4 +1,8 @@
-import { FacebookAuthProvider, signInWithPopup , TwitterAuthProvider  } from "firebase/auth";
+import {
+  FacebookAuthProvider,
+  signInWithPopup,
+  TwitterAuthProvider,
+} from "firebase/auth";
 import { collection, doc, getDocs, setDoc } from "firebase/firestore";
 import { auth, db } from "../../shared/firebase";
 
@@ -17,25 +21,11 @@ export const SignInWithProvder = (provider: any, type: string) => {
 
     if (isStored) return;
 
-    let token;
-    if (type === "facebook") {
-      // If logined with facebook, I need to store additional info about "token" because I can only get profile picture "photoURL" from FB API when I add "?access_token={someToken}", so I store that "someToken" is my FireStore
-      const credential = FacebookAuthProvider.credentialFromResult(result);
-      token = credential?.accessToken;
-    }
-
-   
-
     setDoc(doc(db, "users", user.uid), {
       fullname: user.displayName,
-
-      ...(type === "google" && { photoUrl: user.photoURL }),
-      ...(type === "facebook" && {
-        photoUrl: user.photoURL + "?access_token=" + token,
-      }),
+      photoUrl: user.photoURL,
       bookmarks: [],
       recentlyWatch: [],
-      ...(type === "facebook" && { token }),
     });
   });
 };
