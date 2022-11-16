70169ddb074c45757fd505787bc1d2753413343b
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-16 06:59:42 +0100

    got the user using appselector

--- src/components/Profile/ProfileImage.tsx
+++ src/components/Profile/ProfileImage.tsx
@@ -1,11 +1,13 @@
 import { FunctionComponent } from "react";
-
+import  {useAppSelector} from '../../store/hooks'
+ 
 interface ProfileImageProps {}
 
 const ProfileImage: FunctionComponent<ProfileImageProps> = () => {
+    const currentUser = useAppSelector(state => state.user.user)
   return (
      <div className="shrink-0 md:max-w-[450px] w-full border-solid border-black">
-         
+          
      </div>
   );
 };
