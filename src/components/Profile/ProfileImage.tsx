454e403d473ccd43ec2d598603565438cbcae59c
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-16 07:06:01 +0100

    profile image added

--- src/components/Profile/ProfileImage.tsx
+++ src/components/Profile/ProfileImage.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import  {useAppSelector} from '../../store/hooks'
  
 interface ProfileImageProps {}
@@ -7,7 +8,12 @@ const ProfileImage: FunctionComponent<ProfileImageProps> = () => {
     const currentUser = useAppSelector(state => state.user.user)
   return (
      <div className="shrink-0 md:max-w-[450px] w-full border-solid border-black">
-          
+         <h1> { currentUser?.displayName}</h1>
+         <LazyLoadImage
+           src="/Images/user.svg"
+           alt="profile-image" 
+           className="h-[200px]  w-[200px] object-cover rounded-full"
+         />  
      </div>
   );
 };
