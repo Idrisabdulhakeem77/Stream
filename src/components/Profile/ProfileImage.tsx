bbd11479ba6115f0ead70a77353e5dfcacdff89c
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-16 07:11:39 +0100

    added delte button

--- src/components/Profile/ProfileImage.tsx
+++ src/components/Profile/ProfileImage.tsx
@@ -1,20 +1,24 @@
 import { FunctionComponent } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import  {useAppSelector} from '../../store/hooks'
- 
+import { useAppSelector } from "../../store/hooks";
+import { BsTrash } from "react-icons/bs";
+
 interface ProfileImageProps {}
 
 const ProfileImage: FunctionComponent<ProfileImageProps> = () => {
-    const currentUser = useAppSelector(state => state.user.user)
+  const currentUser = useAppSelector((state) => state.user.user);
   return (
-     <div className="shrink-0 md:max-w-[450px] w-full border-solid border-black">
-         <h1> { currentUser?.displayName}</h1>
-         <LazyLoadImage
-           src="/Images/user.svg"
-           alt="profile-image" 
-           className="h-[200px]  w-[200px] object-cover rounded-full"
-         />  
-     </div>
+    <div className="shrink-0 md:max-w-[450px] w-full border-b-2 border-black">
+      <h1> {currentUser?.displayName}</h1>
+      <LazyLoadImage
+        src="/Images/user.svg"
+        alt="profile-image"
+        className="h-[200px]  w-[200px] object-cover rounded-full"
+      />
+      <button className="absolute top-12 left-48">
+        <BsTrash size={20} />
+      </button>
+    </div>
   );
 };
 
