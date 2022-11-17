900724b01b7ac3d91a0c5b8840522d7b80bb8832
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-17 07:48:36 +0100

    added video interface

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -214,6 +214,19 @@ export interface Reviews {
   url: string;
 }
 
+export interface Video {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
+
 
 export interface FilmInfo {
   detail?: DetailMovie | DetailTV | undefined;
