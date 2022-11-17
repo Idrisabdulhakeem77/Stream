9681a3e9862442876fe78606dd912e766aee50c2
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-17 07:49:34 +0100

    added episode interface

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -226,6 +226,44 @@ export interface Video {
   published_at: string;
   id: string;
 }
+export interface Episode {
+  air_date: string;
+  episode_number: number;
+  id: number;
+  name: string;
+  overview: string;
+  production_code: string;
+  season_number: number;
+  still_path: string;
+  vote_average: number;
+  vote_count: number;
+  crew: {
+    department: string;
+    job: string;
+    credit_id: string;
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+  }[];
+  guest_stars: {
+    credit_id: string;
+    order: number;
+    character: string;
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+  }[];
+}
 
 
 export interface FilmInfo {
