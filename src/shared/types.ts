89a170d67f006bda13bfe3c198c8dff4c26493a8
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-17 07:01:54 +0100

    added the detailMovie interface to type.ts

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -80,5 +80,39 @@ export interface AnimeItempage {
       current_page : number,
        items : { count : number , per_page : number , total : number }
      }
-     data : Animes[]
+     data : Animes[] | undefined
+}
+
+export interface DetailMovie {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: any;
+  budget: number;
+  genres: { id: number; name: string }[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: any;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: { iso_3166_1: string; name: string }[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: { iso_639_1: string; name: string; english_name: string }[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  media_type?: "movie";
 }
