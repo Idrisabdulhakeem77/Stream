86c0cfaffd83dee122f0a5f985829766ac84ec8e
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-18 20:38:15 +0100

    creted the filmInfoComponent

---
+++ src/components/Common/FilmInfo.tsx
@@ -0,0 +1,15 @@
+import {FC} from 'react'
+
+
+interface FilmInfoProps {} 
+
+
+const FilmInfo :FC<FilmInfoProps> =  ( ) => {
+     return (
+         <div>
+             Film Info
+         </div>
+     )
+}
+
+export default FilmInfo
