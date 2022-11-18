dbdeb7ce73aaf734e361fcd563d95e682c02b45c
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-18 21:00:29 +0100

    added sidebar component to filmDetail comp

--- src/components/Common/FilmDetail.tsx
+++ src/components/Common/FilmDetail.tsx
@@ -2,6 +2,9 @@ import { FC, useState } from "react";
 import { DetailMovie, DetailTV, FilmInfo } from "../../shared/types";
 import { useAppSelector } from "../../store/hooks";
 import Title from "./Title";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import Sidebar from "./Sidebar";
 
 const FilmDetail: FC<FilmInfo> = ({
   similar,
@@ -17,7 +20,24 @@ const FilmDetail: FC<FilmInfo> = ({
      <>
        { detail ? <Title value={`${( detail as DetailTV).name || (detail as DetailMovie).title} | Stream`}/> : null}
 
-       
+       <div className="flex justify-between items-center my-4 px-4 md:hidden">
+        <Link to="/">
+          <div className="uppercase font-medium text-lg tracking-widest">
+            {" "}
+            AnimeStream{" "}
+          </div>
+        </Link>
+        <button onClick={() => setIsSidebarActive((prevState) => !prevState)}>
+          <FaBars size={25} />
+        </button>
+      </div>
+
+      <div className="flex items-start">
+        <Sidebar
+          isSidebarOpen={isSidebarActive}
+          setIsSidebarOpen={setIsSidebarActive}
+        />
+        </div>
      </>
   );
 };
