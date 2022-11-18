0c1f145adf611012201ff74d18bdc695cd91bdca
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-18 20:40:37 +0100

    renamed fimInfo to Filmdetail

--- src/pages/Movies/MovieDetail.tsx
+++ src/pages/Movies/MovieDetail.tsx
@@ -3,6 +3,8 @@ import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 import {getFullMovieDetails} from '../../services/movie'
 import { FilmInfo} from "../../shared/types"
+import FilmInfo from '../../components/Common/FilmDetail'
+
 
 interface MovieDetailProps {}
 
@@ -11,16 +13,14 @@ interface MovieDetailProps {}
 const MovieDetail : FC<MovieDetailProps> = () => {
     const { id } = useParams()
     
-    const {data : MovieInfoData , error , isError} = useQuery<FilmInfo | undefined>( ['movie-info'] ,  () => getFullMovieDetails(Number(id as string)))  
+    const {data , error , isError} = useQuery<FilmInfo | undefined>( ['movie-info'] ,  () => getFullMovieDetails(Number(id as string)))  
 
-
+     console.log(data)
     if(isError) return <div> Error </div>
      
 
    return (
-      <div>
-          Movie Detail
-      </div>
+      <FilmInfo/>
    )
 }
 
