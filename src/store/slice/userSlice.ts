2c33cde0b39f6505b357371b41eaed64892b6677
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-18 21:07:19 +0100

    removed setGuestUser action from userSlice

--- src/store/slice/userSlice.ts
+++ src/store/slice/userSlice.ts
@@ -18,23 +18,13 @@ export const userSlice = createSlice({
   reducers: {
     setCurrentUser : (state , action : PayloadAction<User | null>) => {
           state.user = action.payload
-    } ,
-     
-    setGuestUser : ( state , action ) => {
-        state.user = {
-           displayName : "Guest" ,
-           email : "guest@gmail.com" ,
-           emailVerified : true,
-           photoURL: "https://i.ibb.co/stB42Nb/catface-5.jpg" ,
-           uid: "D3xcrHOuQ7fI2kPPH8eljwUrqcH2"
-        }
-    }
+    } 
   },
 });
 
 
 
-export const { setCurrentUser  , setGuestUser} = userSlice.actions;
+export const { setCurrentUser } = userSlice.actions;
 
 export default userSlice.reducer;
 
