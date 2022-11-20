6390abb64dbf6446042910d8e5a9957b398f356f
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-20 17:26:06 +0100

    imported useSearchparams

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent } from "react";
+import  {useSearchParams} from 'react-router-dom'
 
 interface SearchProps {}
 
 
 const Search : FunctionComponent<SearchProps> = () => {
+    const [searchParams  , setSearchParams] = useSearchParams()
       return (
          <div>
              Search page
