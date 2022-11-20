45aa2cd9e20f89574e6bd9975908cb9b90caf12e
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-20 17:58:06 +0100

    removed searchbar

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,15 +3,16 @@ import { useSearchParams } from "react-router-dom";
 import Title from "../components/Common/Title";
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
-import   { useCurrentViewPort } from '../components/hooks/useCurrentViewPort'
+import { useCurrentViewPort } from "../components/hooks/useCurrentViewPort";
 import Sidebar from "../components/Common/Sidebar";
+import SearchBox from "../components/Common/SearchBox";
 
 interface SearchProps {}
 
 const Search: FunctionComponent<SearchProps> = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [isSidebarActive, setIsSidebarActive] = useState(false);
-  const  {isMobile }= useCurrentViewPort()
+  const { isMobile } = useCurrentViewPort();
 
   const query = searchParams.get("query");
   return (
@@ -31,19 +32,23 @@ const Search: FunctionComponent<SearchProps> = () => {
       </div>
 
       <div className="flex">
-          <Sidebar isSidebarOpen={isSidebarActive} setIsSidebarOpen={setIsSidebarActive}/>
+        <Sidebar
+          isSidebarOpen={isSidebarActive}
+          setIsSidebarOpen={setIsSidebarActive}
+        />
 
-           <div className="flex-grow">
-              
-           </div>
+        <div className="flex-grow">
+          <div className="">
+            {/* <SearchBox autoFocus /> */}
+          </div>
+        </div>
 
-           { !isMobile &&  (
-              <div  className="shrink-0 md:max-w-[310px] w-full pt-4 px-3"> 
-                  Last Column
-              </div>
-           )}
+        {!isMobile && (
+          <div className="shrink-0 md:max-w-[310px] w-full pt-4 px-3">
+            Last Column
+          </div>
+        )}
       </div>
-      
     </>
   );
 };
