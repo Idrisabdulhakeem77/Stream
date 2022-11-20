fdc3eaf2c713d27f373352736a394388f0a62035
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-20 16:54:26 +0100

    fixe bug in filmdetail.tsx

--- src/components/FilmDetail/FilmDetail.tsx
+++ src/components/FilmDetail/FilmDetail.tsx
@@ -7,6 +7,7 @@ import { FaBars } from "react-icons/fa";
 import Sidebar from "../Common/Sidebar";
 import Skeleton from "../Common/Skeleton";
 import { resizeImage } from "../../shared/utils";
+import { BsPlay } from "react-icons/bs";
 
 const FilmDetail: FC<FilmInfo> = ({
   similar,
@@ -15,14 +16,21 @@ const FilmDetail: FC<FilmInfo> = ({
   videos,
   reviews,
 }) => {
-     const currentUser = useAppSelector(state => state.user.user)
-     const [isBookMarked , setIsBookMarked] = useState(false)
-     const [isSidebarActive , setIsSidebarActive] = useState(false)
+  const currentUser = useAppSelector((state) => state.user.user);
+  const [isBookMarked, setIsBookMarked] = useState(false);
+  const [isSidebarActive, setIsSidebarActive] = useState(false);
   return (
-     <>
-       { detail ? <Title value={`${( detail as DetailTV).name || (detail as DetailMovie).title} | Stream`}/> : null}
+    <>
+      {detail ? (
+        <Title
+          value={`${
+            (detail as DetailTV).name || (detail as DetailMovie).title
+          } | Stream`}
+        />
+      ) : null}
 
-       <div className="flex justify-between items-center my-4 px-4 md:hidden">
+      <div className="flex justify-between items-center my-4 px-4 md:hidden">
+      <div className="bg-gradient-to-br from-transparent to-black/70 h-full rounded-bl-2xl">
         <Link to="/">
           <div className="uppercase font-medium text-lg tracking-widest">
             AnimeStream
@@ -38,38 +46,56 @@ const FilmDetail: FC<FilmInfo> = ({
           isSidebarOpen={isSidebarActive}
           setIsSidebarOpen={setIsSidebarActive}
         />
-         <div className="flex-grow ">
-             {!detail ? <Skeleton className=""/> : null}
-             {detail ? (
-                <div style={{ backgroundImage : `url(${resizeImage(detail.backdrop_path )})`}}
-                  className="bg-cover bg-no-repeat bg-center md:h-[400px] h-[300px] relative "
-                 >
-                   <div className="text-white" id="image-container">
-                      <h1> { ( detail as DetailTV).name || (detail as DetailMovie).title } </h1>
-                      <div className="flex justify-between">
-                         <div>
-                           { detail.genres.map(genre => (
-                              <li key={genre.id} className="mb-2 list-none">
-                                 { genre.name}
-                              </li>
-                           )) }
-                         </div>
-                         <div>
-                             {`${( detail as DetailMovie).vote_average.toFixed(1)} / 10 `}
-                          </div>   
-                      </div>
-                      <div>
-                       <p>
-                         {(detail as DetailMovie).overview}
-                       </p>
-                    </div>
-                    </div>
-                    
+        <div className="flex-grow ">
+          {!detail ? <Skeleton className="" /> : null}
+          {detail ? (
+            <div
+              style={{
+                backgroundImage: `url(${resizeImage(detail.backdrop_path)})`,
+              }}
+              className="bg-cover bg-no-repeat bg-center md:h-[450px] h-[300px] relative"
+            >
+              <div
+                className="text-white w-[400px] absolute top-9 font-extrabold right-10"
+                id="image-container"
+              >
+                <h1 className="text-[50px] mb-4">
+                  {" "}
+                  {(detail as DetailTV).name ||
+                    (detail as DetailMovie).title}{" "}
+                </h1>
+                <div className="flex gap-2">
+                  <div className="flex gap-2">
+                    {detail.genres.map((genre) => (
+                      <li key={genre.id} className="mb-4 list-none">
+                        {genre.name}
+                      </li>
+                    ))}
+                  </div>
+                  <div>
+                    {`${(detail as DetailMovie).vote_average.toFixed(1)} / 10 `}
+                  </div>
                 </div>
-             ) : null}
-         </div>
+                <div>
+                  <p>{(detail as DetailMovie).overview}</p>
+                </div>
+                <div className="flex gap-4">
+                  <button className="py-4 px-4 rounded-full bg-black text-white">
+                    {" "}
+                    Add to Bookmark
+                  </button>
+                  <button className="py-4 px-4 bg-black text-white flex gap-2 rounded-full">
+                    <BsPlay size={25}/>
+                    <span> Play Trailer </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          ) : null}
         </div>
-     </>
+        </div>
+      </div>
+    </>
   );
 };
 
