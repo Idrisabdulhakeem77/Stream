b9e560c422ed614e844a3543c16d8ac9107d405c
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-20 17:44:47 +0100

    working on search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,22 +1,51 @@
-import { FunctionComponent } from "react";
-import  {useSearchParams} from 'react-router-dom'
-import Title from "../components/Common/Title"
+import { FunctionComponent, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import Title from "../components/Common/Title";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import   { useCurrentViewPort } from '../components/hooks/useCurrentViewPort'
+import Sidebar from "../components/Common/Sidebar";
 
 interface SearchProps {}
 
+const Search: FunctionComponent<SearchProps> = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [isSidebarActive, setIsSidebarActive] = useState(false);
+  const  {isMobile }= useCurrentViewPort()
 
-const Search : FunctionComponent<SearchProps> = () => {
-    const [searchParams  , setSearchParams] = useSearchParams()
+  const query = searchParams.get("query");
+  return (
+    <>
+      {!query ? <Title value="Search | Stream" /> : null}
+      {query ? <Title value={`Search | ${query} | Steam`} /> : null}
 
-    const query = searchParams.get("query")
-      return (
-         <>
-          {!query ? <Title value="Search | Stream"/> : null}
-          { query ? <Title value={`Search | ${query} | Steam`}/> : null}
-           
-          </>
-      )
-}
+      <div className="flex justify-between items-center my-4 px-4 md:hidden">
+        <Link to="/">
+          <div className="uppercase font-medium text-lg tracking-widest ">
+            AnimeStream
+          </div>
+        </Link>
+        <button onClick={() => setIsSidebarActive((prevState) => !prevState)}>
+          <FaBars size={25} />
+        </button>
+      </div>
 
+      <div className="flex">
+          <Sidebar isSidebarOpen={isSidebarActive} setIsSidebarOpen={setIsSidebarActive}/>
 
-export default Search
+           <div className="flex-grow">
+              
+           </div>
+
+           { !isMobile &&  (
+              <div  className="shrink-0 md:max-w-[310px] w-full pt-4 px-3"> 
+                  Last Column
+              </div>
+           )}
+      </div>
+      
+    </>
+  );
+};
+
+export default Search;
