41bf63759389de84b683b6bd8933fd6b7ae41ba5
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-20 17:30:30 +0100

    adde page title

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,15 +1,20 @@
 import { FunctionComponent } from "react";
 import  {useSearchParams} from 'react-router-dom'
+import Title from "../components/Common/Title"
 
 interface SearchProps {}
 
 
 const Search : FunctionComponent<SearchProps> = () => {
     const [searchParams  , setSearchParams] = useSearchParams()
+
+    const query = searchParams.get("query")
       return (
-         <div>
-             Search page
-         </div>
+         <>
+          {!query ? <Title value="Search | Stream"/> : null}
+          { query ? <Title value={`Search | ${query} | Steam`}/> : null}
+           
+          </>
       )
 }
 
