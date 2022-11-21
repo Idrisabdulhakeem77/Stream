495a4ff62f81676640c9562f252a6f46c0ed643c
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-21 21:22:08 +0100

    finished footer component

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent  } from "react";
-import {BsGithub , BsLinkedin} from 'react-icons/bs'
+import {BsGithub , BsLinkedin , BsFacebook} from 'react-icons/bs'
 
 
 interface FooterProps { }
@@ -7,11 +7,11 @@ interface FooterProps { }
 const Footer : FunctionComponent<FooterProps>  = () => {
      return (
          <> 
-           <div className="flex item-center justify-between" >  
+           <div className="bg-dark-lighten text-white flex justify-between items-center py-5 px-4 shadow-md mt-3 absolute bottom-0 w-full" >  
               <p>
                  <span>Copyright@2022 Design by:Idris Abdulhakeem</span>
               </p>
-              <div>
+              <div className="flex gap-3 items-center">
                  <p className="hidden md:block"> Contact Me</p>
                    <div className="flex gap-3"> 
                       <a href="https://github.com/Idrisabdulhakeem77">
@@ -25,7 +25,7 @@ idris-abdulhakeem
                       </a>
 
                       <a href="https://web.facebook.com/profile.php?id=100073301900842">
-                         <BsGithub size={20}/>
+                         <BsFacebook size={20}/>
                       </a>
                    </div>
                  </div> 
