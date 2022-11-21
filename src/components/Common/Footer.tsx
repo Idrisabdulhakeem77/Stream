e20882154ee76fffc04f74ad7a55313776346a7f
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-21 21:29:28 +0100

    finished footer

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,39 +1,38 @@
-import { FunctionComponent  } from "react";
-import {BsGithub , BsLinkedin , BsFacebook} from 'react-icons/bs'
+import { FunctionComponent } from "react";
+import { BsGithub, BsLinkedin, BsFacebook } from "react-icons/bs";
 
+interface FooterProps {}
 
-interface FooterProps { }
+const Footer: FunctionComponent<FooterProps> = () => {
+  return (
+    <>
+      <div className="bg-dark-lighten text-white flex justify-between items-center py-5 px-4 shadow-md mt-3 absolute bottom-0 w-full">
+        <p>
+          <span>Copyright@2022 Design by:Idris Abdulhakeem</span>
+        </p>
+        <div className="flex gap-3 items-center">
+          <p className="hidden md:block"> Contact Me</p>
+          <div className="flex gap-3">
+            <a href="https://github.com/Idrisabdulhakeem77">
+              <BsGithub size={20} />
+            </a>
 
-const Footer : FunctionComponent<FooterProps>  = () => {
-     return (
-         <> 
-           <div className="bg-dark-lighten text-white flex justify-between items-center py-5 px-4 shadow-md mt-3 absolute bottom-0 w-full" >  
-              <p>
-                 <span>Copyright@2022 Design by:Idris Abdulhakeem</span>
-              </p>
-              <div className="flex gap-3 items-center">
-                 <p className="hidden md:block"> Contact Me</p>
-                   <div className="flex gap-3"> 
-                      <a href="https://github.com/Idrisabdulhakeem77">
-                         <BsGithub size={20}/>
-                      </a>
-
-                      <a href="https://www.linkedin.com/in/ 
+            <a
+              href="https://www.linkedin.com/in/ 
 idris-abdulhakeem
-">
-                         <BsLinkedin size={20}/>
-                      </a>
+"
+            >
+              <BsLinkedin size={20} />
+            </a>
 
-                      <a href="https://web.facebook.com/profile.php?id=100073301900842">
-                         <BsFacebook size={20}/>
-                      </a>
-                   </div>
-                 </div> 
-           </div>
-         </>
-     )
+            <a href="https://web.facebook.com/profile.php?id=100073301900842">
+              <BsFacebook size={20} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    
-}
-
-export default Footer
+export default Footer;
