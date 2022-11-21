5612927b04c949c97ba079b54ac30f9624de251c
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-21 21:06:49 +0100

    added contact info

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent  } from "react";
+import {BsGithub , BsLinkedin} from 'react-icons/bs'
 
 
 interface FooterProps { }
@@ -6,7 +7,29 @@ interface FooterProps { }
 const Footer : FunctionComponent<FooterProps>  = () => {
      return (
          <> 
-          Footer
+           <div className="flex item-center justify-between" >  
+              <p>
+                 <span>Copyright@2022 Design by:Idris Abdulhakeem</span>
+              </p>
+              <div>
+                 <p className="hidden md:block"> Contact Me</p>
+                   <div className="flex gap-3"> 
+                      <a href="https://github.com/Idrisabdulhakeem77">
+                         <BsGithub size={20}/>
+                      </a>
+
+                      <a href="https://www.linkedin.com/in/ 
+idris-abdulhakeem
+">
+                         <BsLinkedin size={20}/>
+                      </a>
+
+                      <a href="https://web.facebook.com/profile.php?id=100073301900842">
+                         <BsGithub size={20}/>
+                      </a>
+                   </div>
+                 </div> 
+           </div>
          </>
      )
 
