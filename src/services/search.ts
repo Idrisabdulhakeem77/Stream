de819f7174b2a908ea38d893ca28111f29662010
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-11-22 06:59:31 +0100

    added getSearchResult function to search.ts

--- src/services/search.ts
+++ src/services/search.ts
@@ -1,25 +1,40 @@
-import { getRecommendedGenreType } from "../shared/types"
-import axios from '../shared/axios'
+import { getRecommendedGenreType, ItemsPage } from "../shared/types";
+import axios from "../shared/axios";
 
-export const getRecommendedGenre = async() : Promise<getRecommendedGenreType> => {
-    const movieGenres =  (await axios.get('/genre/movie/list')).data.genres ;
-    const tvGenres =  ( await axios.get('/genre/tv/list')).data.genres ;
+export const getRecommendedGenre =
+  async (): Promise<getRecommendedGenreType> => {
+    const movieGenres = (await axios.get("/genre/movie/list")).data.genres;
+    const tvGenres = (await axios.get("/genre/tv/list")).data.genres;
 
     return {
-         movieGenres , tvGenres
-    }    
-} 
+      movieGenres,
+      tvGenres,
+    };
+  };
 
+export const getSearchKeyWord = async (query: string): Promise<string[]> => {
+  return (
+    await axios.get("/search/keyword", {
+      params: {
+        query,
+      },
+    })
+  ).data.results
+    .map((item: any) => item.name)
+    .filter((_: any, index: number) => index < 5);
+};
 
-export const getSearchKeyWord = async(query : string) : Promise<string[]>  => {
-     return (
-         await axios.get('/search/keyword' , {
-             params : {
-                 query
-             }
-         })
-     ).data.results
-     .map( (item : any) => item.name )
-     .filter(( _ : any , index : number) => index < 5) 
-     
-}
+export const getSearchResult: (
+  page,
+  search,
+  query
+) => Promise<ItemsPage> = async (page, search, query) => {
+  const data = (
+    await axios.get(`/search/${search}`, {
+      params: {
+        page,
+        query,
+      },
+    })
+  ).data;
+};
