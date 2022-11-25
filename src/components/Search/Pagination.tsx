dbcf36a57155314e5adad6721d33f2aead600e43
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-11-25 21:36:48 +0100

    created the pagination component

---
+++ src/components/Search/Pagination.tsx
@@ -0,0 +1,16 @@
+import { FunctionComponent } from "react";
+
+
+interface PaginationProps {}
+
+
+const Pagination : FunctionComponent<PaginationProps> = () => {
+      return (
+         <div>
+             
+         </div>
+      )
+}
+
+
+export default Pagination
