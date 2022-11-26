f9b49186fa84290a11a21f9dd1be0a2b1fd3bca5
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-11-26 08:44:23 +0100

    created BookMarkResults Component

---
+++ src/components/Bookmark/BookmarkResults.tsx
@@ -0,0 +1,16 @@
+import { FunctionComponent } from "react";
+
+
+interface BookmarkResultsProps {} 
+
+
+const BookmarkResults :  FunctionComponent<BookmarkResultsProps> = () => {
+     return ( 
+        <div>
+            bookmark result 
+        </div>
+     )
+}
+
+
+export default BookmarkResults
