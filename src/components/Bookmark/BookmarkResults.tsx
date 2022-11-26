30e13791c5dba406e4a3b531f22bd6e86e427d6f
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-11-26 17:29:37 +0100

    add condition for when film lenght is 0

--- src/components/Bookmark/BookmarkResults.tsx
+++ src/components/Bookmark/BookmarkResults.tsx
@@ -2,33 +2,44 @@ import { FunctionComponent } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Items } from "../../shared/types";
 
-
 interface BookmarkResultsProps {
-    films : Items[],
-    isEditting : boolean 
-    pageType  : string
-    selections : number[]
-    setSelection : any
-    isLoading: boolean
-} 
-
-
-const BookmarkResults :  FunctionComponent<BookmarkResultsProps> = ( { films , isEditting ,isLoading,pageType ,selections , setSelection} ) => {
-     return ( 
-        <>
-           { films.length === 0 && !isLoading && (
-             <div>
-                 <div> 
-                    <LazyLoadImage
-                    src="/error.png"
-                      effect="opacity"
-                     />
-                 </div>
-             </div>
-           )}
-         </>
-     )
+  films: Items[];
+  isEditting: boolean;
+  pageType: string;
+  selections: number[];
+  setSelection: any;
+  isLoading: boolean;
 }
 
+const BookmarkResults: FunctionComponent<BookmarkResultsProps> = ({
+  films,
+  isEditting,
+  isLoading,
+  pageType,
+  selections,
+  setSelection,
+}) => {
+  return (
+    <>
+      {films.length === 0 && !isLoading && (
+        <div className="text-white text-2xl text-center col-span-full mt-10">
+          <div className=" flex justify-center">
+            <LazyLoadImage
+              src="/error.png"
+              effect="opacity"
+              alt="error"
+              className="w-[600px] object-cover"
+            />
+          </div>
+          <p className="mt-5">
+            {pageType === "bookmark"
+              ? "Your bookmark list for this type is Empty Empty."
+              : "Your recently watched films for this type is empty. "}
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
 
-export default BookmarkResults
+export default BookmarkResults;
