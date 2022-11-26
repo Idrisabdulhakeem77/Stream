a65d3be307e7bde311667a447428eb7897e24dcd
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-11-26 08:42:21 +0100

    dne the neccesary import

--- src/components/Bookmark/FilmListForBookmarkedAndRecent.tsx
+++ src/components/Bookmark/FilmListForBookmarkedAndRecent.tsx
@@ -1,5 +1,12 @@
 import { FunctionComponent } from "react";
+import {useAutoAnimate} from "@formkit/auto-animate/react"
+import { useAppSelector } from '../../store/hooks'
 import { Items } from "../../shared/types";
+import Sidebar from '../Common/Sidebar'
+import Skeleton from '../Common/Skeleton'
+import { updateDoc , doc} from "firebase/firestore"
+import { Link } from "react-router-dom";
+import {db} from '../../shared/firebase'
 
 interface FilmListForBookmarkedAndRecentProps {
   films: Items[];
@@ -10,6 +17,10 @@ interface FilmListForBookmarkedAndRecentProps {
 const FilmListForBookmarkedAndRecent: FunctionComponent<
   FilmListForBookmarkedAndRecentProps
 > = ({ films, loading, pageType }) => {
+    
+    const user = useAppSelector(state => state.user.user)
+
+   const [parent] = useAutoAnimate()
   return <div>Something</div>;
 };
 
