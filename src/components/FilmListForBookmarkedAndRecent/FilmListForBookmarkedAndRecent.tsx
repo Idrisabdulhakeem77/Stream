e0bae5da3933069969a59b157b1b4fead52b98a8
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-11-26 10:46:57 +0100

    feat : clear selection button in prompt

--- src/components/FilmListForBookmarkedAndRecent/FilmListForBookmarkedAndRecent.tsx
+++ src/components/FilmListForBookmarkedAndRecent/FilmListForBookmarkedAndRecent.tsx
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 import { db } from "../../shared/firebase";
 import { AiOutlineDelete } from "react-icons/ai";
 import { FaBars } from "react-icons/fa";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 interface FilmListForBookmarkedAndRecentProps {
   films: Items[];
@@ -23,10 +24,30 @@ const FilmListForBookmarkedAndRecent: FunctionComponent<
   const [isShowPrompt, setIsShowPrompt] = useState(false);
   const [selections, setSelections] = useState<number[]>([]);
   const [isSidebarActive, setIsSidebarActive] = useState(false);
+  const [currentTab , setCurrentTab] = useState(localStorage.getItem("bookmarkCurrentTab" ) || "all") 
+  const [isSelectAll , setIsSelectAll] = useState(false)
 
   const [parent] = useAutoAnimate();
   const [show] = useAutoAnimate();
   const [action] = useAutoAnimate();
+
+   const clearSelection = () => {
+       if(!user) return
+
+       const editedFilms = films.filter(
+        (film) => selections.indexOf(film.id) === -1
+      );
+
+      updateDoc(doc(db, "users", user?.uid), {
+        ...(pageType === "bookmark" && { bookmarks: editedFilms.reverse() }),
+        ...(pageType === "recent" && { recentlyWatch: editedFilms.reverse() }),
+      });
+
+      setSelections([]);
+      setIsSelectAll(false);
+      setIsShowPrompt(false);
+   }
+   
   return (
     <>
       <div
@@ -53,7 +74,9 @@ const FilmListForBookmarkedAndRecent: FunctionComponent<
               >
                 Cancel
               </button>
-              <button className="px-6 py-1 rounded-md text-white bg-red-500 hover:bg-red-600 transition duration-300">
+              <button 
+               onClick={clearSelection}
+              className="px-6 py-1 rounded-md text-white bg-red-500 hover:bg-red-600 transition duration-300">
                 Yes
               </button>
             </div>
@@ -73,6 +96,78 @@ const FilmListForBookmarkedAndRecent: FunctionComponent<
           <button onClick={() => setIsSidebarActive((prev) => !prev)}>
             <FaBars size={25} />
           </button>
+        </div>
+
+        <div className="md:flex hidden gap-6 items-center absolute top-4 right-5">
+          <p> {user?.displayName || "Unknown"}</p>
+          <LazyLoadImage
+            src={user ? (user.photoURL as string) : "/user.svg"}
+            alt="User avatar"
+            className="w-10 h-10 rounded-full object-cover"
+            effect="opacity"
+            referrerPolicy="no-referrer"
+          />
+        </div>
+        <div className="flex">
+          <Sidebar
+            isSidebarOpen={isSidebarActive}
+            setIsSidebarOpen={setIsSidebarActive}
+          />
+          <div className="flex-grow  px-[2vw] pb-16 pt-7 min-h-screen">
+            <h1 className="uppercase  font-semibold text-[25px] mb-4  ">
+              {pageType === "bookmark"
+                ? "My favourite films"
+                : "Films I Watched"}
+            </h1>
+            <div
+              // @ts-ignore
+              ref={parent}
+              className="flex flex-col md:flex-row items-start md:items-end gap-5 md:justify-between m mb-8"
+            > 
+               <div className="inline-flex gap-[30px] pb-[14px] border-b border-gray-darken relative">
+                   <button
+                      onClick={() => {
+                        setCurrentTab("all")
+                        localStorage.setItem("bookmarkCurrentTab" , "all")
+                      }}
+
+                      className={`${
+                        currentTab === "all" &&
+                        "text-white font-medium after:absolute after:bottom-0 after:left-[0%] after:bg-white after:h-[3px] after:w-5"
+                      } transition duration-300 hover:text-white`}
+                    >
+                       All
+                   </button>
+                   <button
+                      onClick={() => {
+                        setCurrentTab("movie")
+                        localStorage.setItem("bookmarkCurrentTab" , "movie")
+                      }}
+                    >
+                       Movie
+                   </button>
+
+                   <button
+                      onClick={() => {
+                        setCurrentTab("tv")
+                        localStorage.setItem("bookmarkCurrentTab" , "tv")
+                      }}
+                    >
+                       Tv
+                   </button>
+
+                   <button
+                      onClick={() => {
+                        setCurrentTab("anime")
+                        localStorage.setItem("bookmarkCurrentTab" , "anime")
+                      }}
+                    >
+                       Anime
+                   </button>
+               </div>
+               
+            </div>
+          </div>
         </div>
       </div>
     </>
