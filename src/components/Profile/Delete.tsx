fe94b3fc2d61b755872b949bec8d76d6a262d5e3
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-11-27 21:08:13 +0100

    created the delte component for profile page

---
+++ src/components/Profile/Delete.tsx
@@ -0,0 +1,15 @@
+
+interface DeleteProps {
+    setIsShowPromptReAuthFor : any
+} 
+
+
+const Delete = ({setIsShowPromptReAuthFor} : DeleteProps) => {
+    return (
+         <>
+          
+         </>
+    )
+}
+
+export default Delete
