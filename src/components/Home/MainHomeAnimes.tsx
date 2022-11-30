94c75fe55138a5195c2317d81d3aaa0ee8815200
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-11-30 21:01:49 +0100

    feat:added anime to home page

--- src/components/Home/MainHomeAnimes.tsx
+++ src/components/Home/MainHomeAnimes.tsx
@@ -1,19 +1,26 @@
-
+import { Animes } from "../../shared/types";
+import AnimeItem from "../Common/AnimeItem";
 
 interface MainHomeAnimesProp {
-    isLoading : boolean,
-    animeData : any
+  isLoading: boolean;
+  animeData: Animes[] | undefined;
 }
 
+const MainHomeAnimes = ({ animeData, isLoading }: MainHomeAnimesProp) => {
+  console.log(animeData);
 
-const MainHomeAnimes = ( {animeData , isLoading} : MainHomeAnimesProp ) => {
-    console.log(animeData)
-     
-     return (
-         <div>
-             Home Animes
-         </div>
-     )
-}
+  return (
+    <>
+      {animeData?.map((anime) => (
+        // grid-cols-sm lg:grid-cols-lg
+        <ul className="grid  gap-x-8 gap-y-10 pt-2">
+          <li key={anime.mal_id}>
+            <AnimeItem key={anime.mal_id} item={anime} />
+          </li>
+        </ul>
+      ))}
+    </>
+  );
+};
 
-export default MainHomeAnimes
+export default MainHomeAnimes;
