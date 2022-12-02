2b0518113c8818fe0e6038b7a367b282f9c33326
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-02 07:40:33 +0100

    created the tvDetails component

---
+++ src/pages/Tv/TvDetails.tsx
@@ -0,0 +1,15 @@
+
+
+
+interface TvDetailsProps {}
+
+
+const TvDetails = () => {
+     return (
+         <div>
+             This is the Tv series details page
+         </div>
+     )
+}
+
+export default TvDetails
