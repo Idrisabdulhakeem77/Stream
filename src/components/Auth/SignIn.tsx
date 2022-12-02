570f3c38dc2a8e63340baa64b97c237375c58952
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-02 07:17:47 +0100

    added error handlers to signin.tsx

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -1,15 +1,13 @@
 import React, { useState, FunctionComponent, useRef, FormEvent } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
-import {  FaGoogle } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { SignInWithProvder } from "./signInWithProvider";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
-import {toast , ToastContainer} from "react-toastify"
+import { toast, ToastContainer } from "react-toastify";
 
-import {
-  GoogleAuthProvider
-} from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
 import { useAppSelector } from "../../store/hooks";
 import ModalNotification from "./ModalNotfication";
 import { auth } from "../../shared/firebase";
@@ -30,41 +28,50 @@ const SignIn: FunctionComponent<SignUpProps> = ({
   const currentUser = useAppSelector((state) => state.user.user);
   const [error, setError] = useState("");
 
- 
+  const signInHandler = (e: FormEvent) => {
+    e.preventDefault();
 
-  const signInHandler = (e : FormEvent) => {
-     e.preventDefault()
-      
-     const email = emailRef.current.value
-    const password = passwordRef.current.value
-     if(!email.trim() || !password.trim()) {
-          toast.error("Input Valid Details" , {
-             autoClose : 3000,
-             pauseOnHover : true,
-             closeOnClick : true,
-             
-          })
-     } 
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    if (!email.trim() || !password.trim()) {
+      toast.error("You Have to Enter Something", {
+        autoClose: 3000,
+        pauseOnHover: true,
+        closeOnClick: true,
+      });
 
-     setisLoading(true)
-     signInWithEmailAndPassword( auth , email , password )
-       .catch( (err)  => {
-          console.log(err)
-       })
-       .finally(() => setisLoading(false)) 
-  }
+      return;
+    }
+
+    setisLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+      .catch((err) => {
+        toast.error("Invaldie Credentials", {
+          autoClose: 3000,
+          closeOnClick: true,
+          hideProgressBar: false,
+          pauseOnHover: true,
+        });
+      })
+      .finally(() => setisLoading(false));
+  };
 
   return (
     <>
-   
-      <ToastContainer/>
-      {currentUser && <ModalNotification message="Sign In succesfully" type="success" />}
+      <ToastContainer />
+      {currentUser && (
+        <ModalNotification message="Sign In succesfully" type="success" />
+      )}
 
-       {/* Add Loader to App */}
-      { Loading &&  <div>Loading.....</div>}
-       { error && (
-         <ModalNotification message="Couldnt Sign in" type="error" setError={setError}/>
-       )} 
+      {/* Add Loader to App */}
+      {Loading && <div>Loading.....</div>}
+      {error && (
+        <ModalNotification
+          message="Couldnt Sign in"
+          type="error"
+          setError={setError}
+        />
+      )}
       <div
         id="form"
         className="max-w-xl w-full min-h-[500px]   absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 border-white"
@@ -82,78 +89,74 @@ const SignIn: FunctionComponent<SignUpProps> = ({
             >
               <FaGoogle size={30} />
             </button>
-           
           </div>
 
           <div className="text-lg"> or use your email to Login:</div>
         </div>
 
-        
-          <form onSubmit={signInHandler}>
-            <div className="px-2 py-3">
-              <div className="relative mb-3">
-                <input
-                  ref={emailRef}
-                  name="email"
-                  type="email"
-                  placeholder="Email Address"
-                  className="w-full bg-dark-lighten px-5 py-4 pr-12 rounded-xl outline-none peer text-white"
-                />
-                <label
-                  htmlFor="email"
-                  className={`absolute left-5 text-gray-400 transition duration-500 pointer-events-none 
+        <form onSubmit={signInHandler}>
+          <div className="px-2 py-3">
+            <div className="relative mb-3">
+              <input
+                ref={emailRef}
+                name="email"
+                type="email"
+                placeholder="Email Address"
+                className="w-full bg-dark-lighten px-5 py-4 pr-12 rounded-xl outline-none peer text-white"
+              />
+              <label
+                htmlFor="email"
+                className={`absolute left-5 text-gray-400 transition duration-500 pointer-events-none 
                 translate-y-[-50%] visible peer-placeholder-shown:opacity-0 peer-placeholder-shown:invisible peer-placeholder-shown:translate-y-[-10%] ease-in-out
                 `}
-                >
-                  Email
-                </label>
-                <MdEmail
+              >
+                Email
+              </label>
+              <MdEmail
+                size={25}
+                className="absolute top-1/2 -translate-y-2/3 right-4"
+              />
+            </div>
+
+            <div className="relative mb-3">
+              <input
+                ref={passwordRef}
+                name="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className="w-full bg-dark-lighten px-5 py-4 pr-12 rounded-xl outline-none peer text-white"
+              />
+              <label
+                htmlFor="pasword"
+                className={`absolute left-5 text-gray-400 transition duration-500 pointer-events-none 
+                translate-y-[-50%] visible peer-placeholder-shown:opacity-0 peer-placeholder-shown:invisible peer-placeholder-shown:translate-y-[-10%] ease-in-out
+                `}
+              >
+                Password
+              </label>
+              {showPassword ? (
+                <AiFillEye
+                  onClick={() => setShowPassword(!showPassword)}
                   size={25}
                   className="absolute top-1/2 -translate-y-2/3 right-4"
                 />
-              </div>
-
-              <div className="relative mb-3">
-                <input 
-                  ref={passwordRef}
-                  name="password"
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Password"
-                  className="w-full bg-dark-lighten px-5 py-4 pr-12 rounded-xl outline-none peer text-white"
+              ) : (
+                <AiFillEyeInvisible
+                  onClick={() => setShowPassword(!showPassword)}
+                  size={25}
+                  className="absolute top-1/2 -translate-y-2/3 right-4"
                 />
-                <label
-                  htmlFor="pasword"
-                  className={`absolute left-5 text-gray-400 transition duration-500 pointer-events-none 
-                translate-y-[-50%] visible peer-placeholder-shown:opacity-0 peer-placeholder-shown:invisible peer-placeholder-shown:translate-y-[-10%] ease-in-out
-                `}
-                >
-                  Password
-                </label>
-                {showPassword ? (
-                  <AiFillEye
-                    onClick={() => setShowPassword(!showPassword)}
-                    size={25}
-                    className="absolute top-1/2 -translate-y-2/3 right-4"
-                  />
-                ) : (
-                  <AiFillEyeInvisible
-                    onClick={() => setShowPassword(!showPassword)}
-                    size={25}
-                    className="absolute top-1/2 -translate-y-2/3 right-4"
-                  />
-                )}
-              </div>
-              <button
-                id="form-child"
-                type="submit"
-                className="px-12 py-3  rounded-full text-xl font-medium hover:bg-dark-lighten transition duration-300  absolute left-1/2 -translate-x-1/2 mt-4 border-2 white"
-              >
-                {" "}
-                Sign in{" "}
-              </button>
+              )}
             </div>
-          </form>
-  
+            <button
+              type="submit"
+              className="px-12 py-3  rounded-full text-xl font-medium hover:bg-dark-lighten transition duration-300  absolute left-1/2 -translate-x-1/2 mt-4 border-2 white"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
         <p className="text-xl flex gap-2 mt-32 justify-center">
           <span>Not a member yet?</span>
           <button
