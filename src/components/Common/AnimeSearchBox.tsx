376bc27655c51f825cd69d9447c8fd3c021221cb
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-03 21:31:58 +0100

    working on animeSearch Input component

--- src/components/Common/AnimeSearchBox.tsx
+++ src/components/Common/AnimeSearchBox.tsx
@@ -1,15 +1,19 @@
-import {FaSearch} from 'react-icons/fa'
-
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 interface AnimeSerachBoxProps {}
 
 const AnimeSearchBox = () => {
+  const [searchInput, setSearchInput] = useState("");
+
   return (
     <div>
       <form>
-        <button className="absolute top-1/2 -translate-y-1/2 left-5 text-white">
+        <button className="absolute top-1/2 -translate-y-1/2 left-5 text-light-gray">
           <FaSearch size={25} className="" />
         </button>
+
+        <input type="text" placeholder="Search..." value={searchInput}  onChange={(e) => setSearchInput(e.target.value)}/>
       </form>
     </div>
   );
