a9b3fd7b7656a4e96d436d90f13e8ebae9a5ae06
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-04 09:11:48 +0100

    finished the ui for anime filter by date

--- src/components/AnimeExplore/AnimeFilterByDate.tsx
+++ src/components/AnimeExplore/AnimeFilterByDate.tsx
@@ -1,16 +1,35 @@
-
-
-
 interface AnimeFilterByDateProps {}
 
-
 const AnimeFilterByDate = () => {
-     return (
-         <div>
-             Filter By Date
-         </div>
-     )
-}
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between items-center">
+        <label htmlFor="from" className="text-lg text-white/70">
+          {" "}
+          From
+        </label>
+        <input
+          type="date"
+          name="from"
+          id="from"
+          className="outline-none bg-dark-lighten-2 px-3 py-1 rounded-md"
+        />
+      </div>
 
+      <div className="flex justify-between items-center">
+        <label htmlFor="to" className=" text-lg text-white/70">
+          {" "}
+          To
+        </label>
+        <input
+          type="date"
+          name="to"
+          id="to"
+          className="outline-none bg-dark-lighten-2 px-3 py-1 rounded-md"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default AnimeFilterByDate
+export default AnimeFilterByDate;
