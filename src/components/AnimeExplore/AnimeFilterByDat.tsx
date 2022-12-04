290fb07616064df7e625f5e61bf39e60c7a4bc59
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-04 09:08:33 +0100

    created the animeFIlterByDate Component

---
+++ src/components/AnimeExplore/AnimeFilterByDat.tsx
@@ -0,0 +1,16 @@
+
+
+
+interface AnimeFilterByDateProps {}
+
+
+const AnimeFilterByDate = () => {
+     return (
+         <div>
+             Filter By Date
+         </div>
+     )
+}
+
+
+export default AnimeFilterByDate
