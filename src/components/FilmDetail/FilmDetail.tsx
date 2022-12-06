4a744a16d74364964e12b4686a27681f905816e2
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-06 06:35:01 +0100

    removed commented code from filmDetails page

--- src/components/FilmDetail/FilmDetail.tsx
+++ src/components/FilmDetail/FilmDetail.tsx
@@ -10,7 +10,7 @@ import { resizeImage } from "../../shared/utils";
 import { BsPlay } from "react-icons/bs";
 import MiniSidebar from "../Common/MiniSidebar";
 import SearchBox from "../Common/SearchBox";
-import {useCurrentViewPort} from "../hooks/useCurrentViewPort"
+import { useCurrentViewPort } from "../hooks/useCurrentViewPort";
 import RightbarFilms from "../Common/RightbarFilms";
 
 const FilmDetail: FC<FilmInfo> = ({
@@ -23,8 +23,8 @@ const FilmDetail: FC<FilmInfo> = ({
   const currentUser = useAppSelector((state) => state.user.user);
   const [isBookMarked, setIsBookMarked] = useState(false);
   const [isSidebarActive, setIsSidebarActive] = useState(false);
-  
-   const {isMobile} = useCurrentViewPort()
+
+  const { isMobile } = useCurrentViewPort();
 
   return (
     <>
@@ -50,57 +50,28 @@ const FilmDetail: FC<FilmInfo> = ({
 
       <div className="flex items-start">
         <MiniSidebar />
+        {isMobile && (
+          <Sidebar
+            isSidebarOpen={isSidebarActive}
+            setIsSidebarOpen={setIsSidebarActive}
+          />
+        )}
         <div className="flex-grow ">
-          {/* {!detail ? <Skeleton className="h-[400px] rounded-bl-2xl>" /> : null}
-          {detail ? (
-            <div
-              style={{
-                backgroundImage: `url(${resizeImage(detail.backdrop_path)})`,
-              }}
-              className="bg-cover bg-no-repeat bg-center md:h-[400px] h-[300px] relative"
-            >  <div className="bg-gradient-to-br from-transparent to-black/70 h-full rounded-bl-2xl">
+          {!detail && (
+            <Skeleton className="h-[400px] rounded-bl-2xl" />
+          )}
+
+           { detail && (
+              <div style={{ backgroundImage : `url(${resizeImage(detail.backdrop_path)})`}}
+                className="bg-no-repeat bg-center bg-cover"
+               >
+
               </div>
-              <div
-                className="text-white w-[400px] h-[300px] absolute top-9 font-extrabold right-10"
-                id="image-container"
-              >
-                <h1 className="text-[40px] mb-4">
-                  {" "}
-                  {(detail as DetailTV).name ||
-                    (detail as DetailMovie).title}{" "}
-                </h1>
-                <div className="flex gap-2">
-                  <div className="flex gap-2">
-                    {detail.genres.map((genre) => (
-                      <li key={genre.id} className="mb-4 list-none">
-                        {genre.name}
-                      </li>
-                    ))}
-                  </div>
-                  <div>
-                    {`${(detail as DetailMovie).vote_average.toFixed(1)} / 10 `}
-                  </div>
-                </div>
-                <div>
-                  <p>{(detail as DetailMovie).overview}</p>
-                </div>
-                <div className="flex gap-4">
-                  <button className="py-4 px-4 rounded-full bg-black text-white">
-                    {" "}
-                    Add to Bookmark
-                  </button>
-                  <button className="py-4 px-4 bg-black text-white flex gap-2 rounded-full">
-                    <BsPlay size={25}/>
-                    <span> Play Trailer </span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          ) : null} */}
+           )}
         </div>
 
         <div className="shrink-0 md:max-w-[310px] w-full relative px-6">
-          { !isMobile &&  <SearchBox />}
+          {!isMobile && <SearchBox />}
           <RightbarFilms
             name="Similar"
             films={similar?.filter((item) => item.id !== detail?.id)}
