ee20a9930c2cbecd1d8340ae5b63d834ce546819
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-11 20:35:45 +0100

    removed unused code

--- src/pages/Anime/AnimeDetails.tsx
+++ src/pages/Anime/AnimeDetails.tsx
@@ -3,21 +3,16 @@ import { useQuery } from "@tanstack/react-query";
 import { getAnimeFullDetails } from "../../services/anime";
 import AnimeDetail from "../../components/Anime/AnimeDetail"
  
-interface AnimeDetailsProps {}
 
 const AnimeDetails = () => {
   const { id } = useParams();
 
-  console.log(id);
   const { data, isError } = useQuery<any, Error>(["anime-details", id], () =>
     getAnimeFullDetails(Number(id as string))
   );
 
-  // console.log(data);
-
-  console.log(data)
-
-
+ 
+   if(isError) return <div> Error  </div>
 
   return (
     <>
