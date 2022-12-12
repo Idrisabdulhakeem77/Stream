44017c0041a9f2d994372fa9cf378195418ca6b7
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-12 08:10:36 +0100

    finished tab button component

--- src/components/FilmDetail/FilmTabInfo.tsx
+++ src/components/FilmDetail/FilmTabInfo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Cast, DetailMovie, DetailTV, Reviews } from "../../shared/types"
 
 interface FilmTabInfoProps {
@@ -7,9 +8,49 @@ interface FilmTabInfoProps {
 }
 
 const FilmTabInfo = ( {detail} : FilmTabInfoProps) => {
+    const [ currentTab , setCurrentTab] = useState("overall")
   return (
     <>
-      <div>Film Tab Info</div>
+     <div className="flex gap-8  text-gray-400 text-lg ">
+        <button
+          className={`hover:text-white transition duration-300 pb-1  ${
+            currentTab === "overall" &&
+            "font-medium -translate-y-2 border-b-2 border-primary text-white"
+          }`}
+          onClick={() => setCurrentTab("overall")}
+        >
+          Overall
+        </button>
+        <button
+          className={`hover:text-white transition duration-300 pb-1 ${
+            currentTab === "cast" &&
+            "font-medium -translate-y-2 border-b-2 border-primary text-white"
+          }`}
+          onClick={() => setCurrentTab("cast")}
+        >
+          Cast
+        </button>
+        <button
+          className={`hover:text-white transition duration-300 pb-1 ${
+            currentTab === "reviews" &&
+            "font-medium -translate-y-2 border-b-2 border-primary text-white"
+          }`}
+          onClick={() => setCurrentTab("reviews")}
+        >
+          Reviews
+        </button>
+        {detail && detail.media_type === "tv" && (
+          <button
+            className={`hover:text-white transition duration-300 pb-1 ${
+              currentTab === "seasons" &&
+              "font-medium -translate-y-2 border-b-2 border-primary text-white"
+            }`}
+            onClick={() => setCurrentTab("seasons")}
+          >
+            Seasons
+          </button>
+        )}
+      </div>
     </>
   );
 };
