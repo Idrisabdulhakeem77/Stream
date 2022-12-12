19a385b40bf440a3c467739ec4e3dddcc893d550
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-12 09:27:25 +0100

    finished te overaall tab

--- src/components/FilmDetail/FilmTabInfo.tsx
+++ src/components/FilmDetail/FilmTabInfo.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Cast, DetailMovie, DetailTV, Reviews } from "../../shared/types";
-import Skeleton from '../../components/Common/Skeleton'
+import Skeleton from "../Common/Skeleton";
+import ReadMore from "../Common/ReadMore";
 
 interface FilmTabInfoProps {
   detail?: DetailMovie | DetailTV | undefined;
@@ -60,10 +61,43 @@ const FilmTabInfo = ({ detail }: FilmTabInfoProps) => {
               <p className="text-xl italic mb-8 text-white text-center">
                 {detail.tagline}
               </p>
-              
             )}
             {!detail && <Skeleton className="h-6 w-[350px] mx-auto mb-8" />}
             <p className="text-white mb-3 font-medium"> STORY </p>
+            {detail && (
+              <ReadMore limitTextNumber={250}>{detail.overview}</ReadMore>
+            )}
+            {!detail && (
+              <>
+                <Skeleton className="h-20" />
+              </>
+            )}
+
+            <p className="text-white font-medium mt-8 mb-3">DETAILS</p>
+            {!detail && (
+              <>
+                <Skeleton className="h-16 w-[40%]" />
+              </>
+            )}
+
+            {detail && (
+              <>
+                <p>Status: {detail.status}</p>
+                {detail.media_type === "movie" && (
+                  <p>Release date: {(detail as DetailMovie).release_date}</p>
+                )}
+                {detail.media_type === "tv" && (
+                  <p>Last air date: {(detail as DetailTV).last_air_date}</p>
+                )}
+                <p>
+                  Spoken language:
+                  {detail.spoken_languages.map(
+                    (language, index) =>
+                      `${index ? ", " : ""} ${language.english_name}`
+                  )}
+                </p>{" "}
+              </>
+            )}
           </>
         )}
       </div>
