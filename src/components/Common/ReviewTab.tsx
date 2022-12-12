194dd2d4bc89e9df561164de214b413373577c44
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-12 10:00:54 +0100

    finished the starRating component

--- src/components/Common/ReviewTab.tsx
+++ src/components/Common/ReviewTab.tsx
@@ -2,27 +2,42 @@ import { Reviews } from "../../shared/types";
 import { useState } from "react";
 import {} from "react-select";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { SortReview } from "../FilmDetail/SortReview";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 interface ReviewTabProps {
-  reviews?: Reviews[]
+  reviews?: Reviews[];
 }
 
 interface ReviewContentProps {
-    reviews: Reviews[];
-    type: string;
-  }
-  
-
-
-const ReviewContent = ( { reviews , type} : ReviewContentProps) => {
-     const [parent] = useAutoAnimate()
-     return(
-         <ul>
-
-         </ul>
-     )
-     
+  reviews: Reviews[];
+  type: string;
 }
+
+const ReviewContent = ({ reviews, type }: ReviewContentProps) => {
+  const [parent] = useAutoAnimate();
+  return (
+    <ul
+      // @ts-ignore
+      ref={parent}
+    >
+      {SortReview(reviews, type).map((review) => (
+        <li key={review.id} className="flex gap-7">
+          <div className="shrink-0 max-w-[60px] w-full h-[60px]">
+            <LazyLoadImage
+              src="/Images/user.svg"
+              alt="reviewer"
+              effect="opacity"
+              className="w-[60px] h-[60px] rounded-full object-cover"
+            />
+          </div>
+
+          
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const ReviewTab = ({ reviews }: ReviewTabProps) => {
   const [reviewSortType, setReviewSortType] = useState("desc");
--- src/components/FilmDetail/StarRating.tsx
+++ src/components/FilmDetail/StarRating.tsx
@@ -1,17 +1,23 @@
+import { FC } from "react";
+import { AiFillStar } from "react-icons/ai";
 
-
-interface StarRatingProps {} 
-
-
-const StarRating = () => {
-     return (
-        <> 
-         <div>
-            Star Rating
-         </div>
-        </>
-     )
+interface StarRatingProps {
+  star: number;
+  maxStar: number;
 }
 
-export default StarRating
- 
+const StarRating: FC<StarRatingProps> = ({ star, maxStar }) => {
+  if (star === 0) return <p></p>;
+  return (
+    <div className="flex gap-[2px]">
+      {[...new Array(maxStar)].map((_, index) => (
+        <AiFillStar
+          key={index}
+          className={`${index < star && "text-primary"} `}
+          size={15}
+        />
+      ))}
+    </div>
+  );
+};
+export default StarRating;
