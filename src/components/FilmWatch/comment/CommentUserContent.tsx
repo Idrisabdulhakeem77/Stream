de174c85784d600cec50c8b69c82aa8c06f45b15
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-13 17:05:06 +0100

    created sortComment function in commentUserContent

--- src/components/FilmWatch/comment/CommentUserContent.tsx
+++ src/components/FilmWatch/comment/CommentUserContent.tsx
@@ -1,4 +1,7 @@
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import { QuerySnapshot, DocumentData } from "firebase/firestore";
+import { useState , useRef  } from "react";
+import { useAppSelector } from "../../../store/hooks";
 
 interface CommentUserContentProps {
   commentData: QuerySnapshot<DocumentData> | null;
@@ -9,8 +12,55 @@ interface CommentUserContentProps {
   role: string;
 }
 
-const CommentUserContent = ({ commentData , commentLimit , media_type ,role , sortType , id}: CommentUserContentProps) => {
-  return <div></div>;
+const CommentUserContent = ({
+  commentData,
+  commentLimit,
+  media_type,
+  role,
+  sortType,
+  id,
+}: CommentUserContentProps) => {
+  const [parent] = useAutoAnimate();
+  const currentUser = useAppSelector((state) => state.user.user);
+  const [commentHidden, setCommentHidden] = useState<string[]>([]);
+  const [showOptionFor, setShowOptionFor] = useState<string | undefined>(
+    undefined
+  );
+  const [editingCommentFor, setEditingCommentFor] = useState<
+    string | undefined
+  >();
+  const editValueRef = useRef<HTMLInputElement>(null!);
+  const [isReplyingFor, setIsReplyingFor] = useState<string | undefined>();
+
+
+  const sortComment = (
+     commentData : QuerySnapshot <DocumentData> | null  ,
+      type : string 
+  ) => {  
+    if (!commentData) return undefined;
+
+    if (type === "popular") {
+      return commentData.docs
+        .slice()
+        .sort(
+          (a, b) =>
+            Object.values(b.data()?.reactions).length -
+            Object.values(a.data()?.reactions).length
+        );
+    }else if ( type === "latest") {
+         return commentData.docs
+    }
+
+  }
+
+  return (
+      <ul
+       //@ts-ignore
+       ref={parent}
+      >
+
+      </ul>
+  );
 };
 
 export default CommentUserContent;
