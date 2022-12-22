bd8635b2ab7637729308ec16b87d015cfb9f3ccd
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-22 20:09:05 +0100

    created the editComment Component

---
+++ src/components/FilmWatch/comment/EditComment.tsx
@@ -0,0 +1,13 @@
+import { useAppSelector } from "../../../store/hooks";
+
+interface EditCommentProps {}
+
+const EditComment = () => {
+  return (
+    <>
+      <div className="relative z-40"></div>
+    </>
+  );
+};
+
+export default EditComment;
