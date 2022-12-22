a4965fa677b4a49154facb63d82c16ef819f76f1
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-22 19:09:35 +0100

    finished the reply component ui

--- src/components/FilmWatch/comment/Reply.tsx
+++ src/components/FilmWatch/comment/Reply.tsx
@@ -1,23 +1,55 @@
 import { db } from "../../../shared/firebase";
 
-import { Timestamp , getDoc , collection } from 'firebase/firestore'
+import { collection, orderBy, query } from "firebase/firestore";
 
 import { useCollectionQuery } from "../../hooks/useCollectionQuery";
+import { useState } from "react";
+import CommentUserData from "./CommentUserData";
 
-
-
-interface ReplyProps { 
-      commentId : string  
+interface ReplyProps {
+  commentId: string;
 }
 
+const Reply = ({ commentId }: ReplyProps) => {
+  const [commentLimit, setCommentLimit] = useState(5);
 
-const Reply = ({ commentId } : ReplyProps) => {
-     return (
-         <div>
-            Reply
-         </div>
-     )
-}
+  const {
+    data: commentData,
+    isLoading,
+    isError,
+  } = useCollectionQuery(
+    commentId,
+    query(collection(db, `replyTo-${commentId}`), orderBy("createdAt", "desc"))
+  );
 
+  return (
+    <>
+      {commentData && commentData.size > 0 && (
+        <div className="mt-5">
+          <CommentUserData
+            // @ts-ignore
+            commentData={commentData}
+            commentLimit={commentLimit}
+            isError={isError}
+            isLoading={isLoading}
+            media_type="replyTo"
+            role="reply"
+            sortType="latest"
+            id={commentId}
+          />
+        </div>
+      )}
 
-export default Reply
+      {commentData && commentData.size > commentLimit && (
+        <button
+          className="font-medium mt-3"
+          onClick={() => setCommentLimit((prevState) => prevState + 5)}
+        >
+          Load more Replies ( {commentLimit} / {commentData.size} )
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Reply;
