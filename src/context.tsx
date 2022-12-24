2499c6fbd120944e292dbffa20448ddee012c76d
Idris Abdulhakeem <idrisabdulhakeem77@gmail.com>
2022-12-24 11:40:54 +0100

    deleted the context api file

--- src/context.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-const AppContext = createContext("");
-
-export const AppProvider = () => {};
-
-export const useGlobalContext = () => {
-  return useContext(AppContext);
-};
